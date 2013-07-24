b55e4177f154524553ce1af2233404a5c97724a9
Damien Pobel <dp@ez.no>
2013-07-24 09:13:59 +0200

    EZP-21310: Added unit tests for eZ.Location class

---
+++ Tests/js/models/assets/ez-locationmodel-tests.js
@@ -0,0 +1,58 @@
+YUI.add('ez-locationmodel-tests', function (Y) {
+
+    var modelTest;
+
+    modelTest = new Y.eZ.ModelTest({
+        name: "eZ Location Model tests",
+
+        init: function () {
+            this.capiMock = new Y.Mock();
+            this.capiGetService = 'getContentService';
+            this.serviceMock = new Y.Mock();
+            this.serviceLoad = 'loadLocation';
+            this.rootProperty = "Location";
+            this.loadResponse = {
+                "Location": {
+                    "_media-type": "application/vnd.ez.api.Location+json",
+                    "_href": "/api/ezp/v2/content/locations/1/2/61",
+                    "id": 61,
+                    "priority": -12,
+                    "hidden": "false",
+                    "invisible": "false",
+                    "ParentLocation": {
+                        "_media-type": "application/vnd.ez.api.Location+json",
+                        "_href": "/api/ezp/v2/content/locations/1/2"
+                    },
+                    "pathString": "/1/2/61/",
+                    "depth": 2,
+                    "childCount": 3,
+                    "remoteId": "a056661abf6a4c778ca3a642797ae5e3",
+                    "Children": {
+                        "_media-type": "application/vnd.ez.api.LocationList+json",
+                        "_href": "/api/ezp/v2/content/locations/1/2/61/children"
+                    },
+                    "Content": {
+                        "_media-type": "application/vnd.ez.api.Content+json",
+                        "_href": "/api/ezp/v2/content/objects/59"
+                    },
+                    "sortField": "PATH",
+                    "sortOrder": "ASC"
+                }
+            };
+        },
+
+        setUp: function () {
+            this.model = new Y.eZ.Location();
+        },
+
+        tearDown: function () {
+            this.model.destroy();
+            delete this.model;
+        }
+
+    });
+
+    Y.Test.Runner.setName("eZ Location Model tests");
+    Y.Test.Runner.add(modelTest);
+
+}, '0.0.1', {requires: ['test', 'model-tests', 'ez-locationmodel', 'ez-restmodel']});
