f197d701ebf8df4790cb1d3f8ac15e4ff4c82f98
Anton Petrushenkov <wiseman.std@gmail.com>
2013-08-05 17:24:23 +0300

    Fix EZP-21182: Implement the FormView view and add it the ContentEditView  !squash   Refactoring

--- Resources/public/js/apps/ez-editorialapp.js
+++ Resources/public/js/apps/ez-editorialapp.js
@@ -128,11 +128,9 @@ YUI.add('ez-editorialapp', function (Y) {
          * @param {Function} next the function to pass control to the next route callback
          */
         handleContentEdit: function (req, res, next) {
-            var vars = this.get('contentEditViewVariables'),
-                form = vars.formView.get('model'),
-                type = vars.contentType;
+            var vars = this.get('contentEditViewVariables');
 
-            form.fieldGroups = type.getFieldGroups();
+            vars.formView.set('contentType', vars.contentType);
 
             this.showView('contentEditView', vars, {
                 update: true,
@@ -320,9 +318,7 @@ YUI.add('ez-editorialapp', function (Y) {
                     mainLocation: new Y.eZ.Location(),
                     owner: new Y.eZ.User(),
                     formView: new Y.eZ.ContentEditFormView({
-                        model : {
-                            fieldGroups : []
-                        }
+                        contentType: {}
                     })
                 }
             }
