f5a82922e40dd287fbb6d16b3872bee80a8f6757
Anton Petrushenkov <wiseman.std@gmail.com>
2013-08-09 18:05:04 +0300

    Fix EZP-21275: As a developer, I want a grunt build script to ease various tasks in the EditorialBundle

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+
+    // Syntax "!<whatever>" means - exclude whatever from the result set
+    var sourceFiles = [
+            "./Resources/public/js/apps/*.js", "!./Resources/public/js/apps/*-min.js",
+            "./Resources/public/js/views/*.js", "!./Resources/public/js/views/*-min.js",
+            "./Resources/public/js/models/*.js", "!./Resources/public/js/models/*-min.js"
+        ],
+        trashFiles = [
+            "./Resources/public/js/apps/*-min.js",
+            "./Resources/public/js/views/*-min.js",
+            "./Resources/public/js/models/*-min.js"
+        ];
+
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: 'jshint.json'
+            },
+            all: sourceFiles
+        },
+        uglify: {
+            all: {
+                files: [
+                    {
+                        expand: true,     // Enable dynamic expansion.
+                        src: sourceFiles,
+                        ext: '-min.js'   // Dest filepaths will have this extension.
+                    }
+                ]
+            }
+        },
+        clean: {
+            all: {
+                src: trashFiles
+            }
+        },
+        instrument: {
+            files : sourceFiles,
+            options : {
+                basePath : 'Tests/instrument/'
+            }
+        },
+        pkg: grunt.file.readJSON('package.json'),
+        yuidoc: {
+            all: {
+                name: '<%= pkg.name %>',
+                description: '<%= pkg.description %>',
+                version: '<%= pkg.version %>',
+                url: '<%= pkg.repository %>',
+                logo: "http://ez.no/extension/ez_ezno/design/ezno_2011/images/ez-logo.png",
+                options: {
+                    paths: [
+                        "./Resources/public/js/apps",
+                        "./Resources/public/js/views",
+                        "./Resources/public/js/models"
+                    ],
+                    outdir: 'api/'
+                }
+            }
+        },
+        shell: {
+            grover: {
+                command: 'grover --server Tests/js/*/*.html',
+                options: {
+                    stdout: true,
+                    stderr: true
+                }
+            },
+            groverCoverage: {
+                command: '  grover --server --coverage -S "?filter=coverage" Tests/js/*/*.html',
+                options: {
+                    stdout: true,
+                    stderr: true
+                }
+            },
+            livedoc: {
+                command: 'yuidoc --server',
+                options: {
+                    stdout: true,
+                    stderr: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-istanbul');
+    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('ugly', ['jshint', 'uglify']);
+    grunt.registerTask('test', ['jshint', 'shell:grover'] );
+    grunt.registerTask('coverage', ['jshint', 'instrument', 'shell:groverCoverage'] );
+    grunt.registerTask('doc', ['yuidoc'] );
+    grunt.registerTask('livedoc', ['shell:livedoc'] );
+
+};
--- README.md
+++ README.md
@@ -26,6 +26,22 @@ This bundle provides the (future) Editorial Interface for eZ Publish.
     assetic:
         bundles: [ eZDemoBundle, EzSystemsEditorialBundle ]
     ```
+* Project maintenance is handled in nodejs environment with a help of Grunt task runner.
+  To install everything properly you should install nodejs first (http://nodejs.org/).
+
+  Next, from project's root directory run ```$ npm install -g grunt-cli``` command.
+  It will install Grunt task runner and make ```$ grunt``` commands available anywhere on your system.
+  Depending on the user setup, this action might require to be root.
+  After that run ```$ npm install``` command . That should make other commands available.
+
+  To be able to edit documentation using live yuidoc server you should also install yuidocjs module by running ```$ npm install -g yuidocjs```.
+
+## Maintenance
+* To execute jshint spell check just run: ```$ grunt lint```
+* You can generate minified versions of main project files.
+Minified files will be situated in the same folder as original ones, but will have "-min" sufffix before the extension.
+Run: ```$ grunt uglify```
+* To delete all minified files, you can run: ```$ grunt clean```
 
 ## Documentation
 
@@ -33,10 +49,16 @@ The API documentation of the JavaScript components can be generated with
 [yuidoc](http://yui.github.io/yuidoc/) by using the following command:
 
 ```
-$ yuidoc -c yuidoc.json
+$ grunt doc
 ```
-
 This will generate a complete API documentation in the `api` directory.
+
+By running 
+```
+$ grunt livedoc
+```
+command you can run documentation server which gives you ability to see documentation changes in real time.
+By default yuidoc documentation server can be accessed on: http://127.0.0.1:3000
 
 ## Tests
 
@@ -45,7 +67,12 @@ This will generate a complete API documentation in the `api` directory.
 To run the JavaScript unit tests, you need to install
 [PhantomJS](http://phantomjs.org) and [grover](http://github.com/yui/grover/).
 Then, the unit tests can be executed with:
-
 ```
-$ grover --server Tests/js/**/*html
+$ grunt test
 ``` 
+
+To run the unit tests and generate a coverage report use:
+```
+$ grunt coverage
+```
+Beware, this command will be executed much slower, since instrumented versions of the js files will be used. You can also run ```$ grunt instrument``` for simple update  of instrumented files.
