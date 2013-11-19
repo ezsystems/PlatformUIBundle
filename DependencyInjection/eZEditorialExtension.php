01fb9c149ec4f5d417ef7288972299e22cbce3e9
StephaneDiot <sdiot@live.fr>
2013-11-19 11:36:35 +0100

    Implement EZP-21702: Added a twig function to generation te YUI module config
    Set a name on the bundle so that the asset path is easy to compute.

--- DependencyInjection/eZEditorialExtension.php
+++ DependencyInjection/eZEditorialExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class EzSystemsEditorialExtension extends Extension
+class eZEditorialExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -24,5 +24,6 @@ class EzSystemsEditorialExtension extends Extension
 
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
         $loader->load( 'services.yml' );
+        $loader->load( 'yui.yml' );
     }
 }
