fd7c8c79d06e9432a637c25a141d687523e77f7f
Anton Petrushenkov <wiseman.std@gmail.com>
2013-11-22 17:58:13 +0200

    Fix EZP-21607: Implement the EmailAddressEditView

---
+++ Tests/js/views/fields/assets/editviewregister-tests.js
@@ -0,0 +1,23 @@
+YUI.add('editviewregister-tests', function (Y) {
+    Y.namespace('eZ');
+
+    Y.eZ.EditViewRegisterTest = {
+        name: "Generic eZ Edit View registration test",
+
+        "Should autoregister": function () {
+            var ViewType = this.viewType,
+                viewKey = this.viewKey,
+                viewName = new ViewType().constructor.NAME;
+
+            try {
+                Y.Assert.areSame(
+                    ViewType,
+                    Y.eZ.FieldEditView.getFieldEditView(viewKey),
+                    "The constructor of " + viewName + " should be registered under " + viewKey + " key"
+                );
+            } catch (e) {
+                Y.Assert.fail(viewName + " is not registered under " + viewKey + " key");
+            }
+        }
+    };
+});
