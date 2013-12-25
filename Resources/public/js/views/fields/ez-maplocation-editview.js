0c9015935ea7ace0c01772ed3e6a4f088e3df43d
Anton Petrushenkov <wiseman.std@gmail.com>
2013-12-25 10:18:36 +0200

    Fix EZP-21822: Implement the MapLocationEditView map-induced location changing methods are working

--- Resources/public/js/views/fields/ez-maplocation-editview.js
+++ Resources/public/js/views/fields/ez-maplocation-editview.js
@@ -8,7 +8,16 @@ YUI.add('ez-maplocation-editview', function (Y) {
 
     Y.namespace('eZ');
 
-    var FIELDTYPE_IDENTIFIER = 'ezgmaplocation';
+    var FIELDTYPE_IDENTIFIER = 'ezgmaplocation',
+        DEFAULT_LATITUDE_WITHOUT_LOCATION = 0,
+        DEFAULT_LONGITUDE_WITHOUT_LOCATION = 0,
+        DEFAULT_ZOOM_WITHOUT_LOCATION = 1,
+        DEFAULT_ZOOM = 8,
+        MARKER_TITLE = "Current location",
+        CONTROL_MAP_TYPE = false,
+        CONTROL_STREET_VIEW = false,
+        LATITUDE_SPAN_CLASS = ".ez-maplocation-latitude",
+        LONGITUDE_SPAN_CLASS = ".ez-maplocation-longitude";
 
     /**
      * Map Location edit view
@@ -25,6 +34,63 @@ YUI.add('ez-maplocation-editview', function (Y) {
             },
             '.ez-maplocation-locate-me-button': {
                 'click': '_locateMe'
+            }
+        },
+
+        /**
+         * Custom initializer method, it initializes google maps (if needed)
+         *
+         * @method initializer
+         */
+        initializer: function () {
+            var that = this;
+
+            if (typeof google != 'object' || typeof google.maps != 'object') {
+                Y.jsonp('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback={callback}', {
+                    on: {
+                        success: function () {
+                            var mapOptions = {
+                                    streetViewControl: CONTROL_STREET_VIEW,
+                                    mapTypeControl: CONTROL_MAP_TYPE
+                                },
+                                map,
+                                marker,
+                                field = that.get('field');
+
+                            // If some location is already defined preparing to
+                            // show it on the map
+                            if (field.fieldValue) {
+                                mapOptions.zoom = DEFAULT_ZOOM;
+                                mapOptions.center = new google.maps.LatLng(field.fieldValue.latitude, field.fieldValue.longitude);
+                            } else {
+                                // Empty location
+                                mapOptions.zoom = DEFAULT_ZOOM_WITHOUT_LOCATION;
+                                mapOptions.center = new google.maps.LatLng(DEFAULT_LATITUDE_WITHOUT_LOCATION, DEFAULT_LONGITUDE_WITHOUT_LOCATION);
+                            }
+
+                            map = new google.maps.Map(
+                                that.get('container').one('.ez-maplocation-map-container').getDOMNode(),
+                                mapOptions
+                            );
+                            that.set('map', map);
+
+                            marker = new google.maps.Marker({
+                                position: mapOptions.center,
+                                map: map,
+                                draggable: true,
+                                title: MARKER_TITLE
+                            });
+                            that.set('marker', marker);
+
+                            google.maps.event.addListener(marker, 'drag', Y.bind(that._markerDrag, that) );
+                            google.maps.event.addListener(map, 'click', Y.bind(that._mapClick, that) );
+
+                        },
+                        failure: function () {
+                            console.log("Throw fatal error here!");
+                        }
+                    }
+                });
             }
         },
 
@@ -49,6 +115,63 @@ YUI.add('ez-maplocation-editview', function (Y) {
         },
 
         /**
+         * Updates the coordinates which are shown to the user in the "dashboard"
+         *
+         * @protected
+         * @method _updateCoordinates
+         */
+        _updateCoordinates: function () {
+            var container = this.get('container');
+            container.one(LATITUDE_SPAN_CLASS).setHTML(this.get('latitude').toFixed(6));
+            container.one(LONGITUDE_SPAN_CLASS).setHTML(this.get('longitude').toFixed(6));
+        },
+
+        /**
+         * Updates the position of the map marker
+         *
+         * @protected
+         * @method _updateMarkerPosition
+         */
+        _updateMarkerPosition: function () {
+            var marker = this.get('marker');
+            marker.setPosition(
+                new google.maps.LatLng(
+                    this.get('latitude'),
+                    this.get('longitude')
+                )
+            );
+        },
+
+        /**
+         * Marker dragging event handler
+         *
+         * @protected
+         * @method _markerDrag
+         * @param {Object} e the event object of the marker drag event
+         */
+        _markerDrag: function (e) {
+            this.set('latitude', e.latLng.lat());
+            this.set('longitude', e.latLng.lng());
+
+            this._updateCoordinates();
+        },
+
+        /**
+         * Marker dragging event handler
+         *
+         * @protected
+         * @method _markerDrag
+         * @param {Object} e the event object of the map click event
+         */
+        _mapClick: function (e) {
+            this.set('latitude', e.latLng.lat());
+            this.set('longitude', e.latLng.lng());
+
+            this._updateCoordinates();
+            this._updateMarkerPosition();
+        },
+
+        /**
          * Defines the variables to imported in the field edit template for text
          * line.
          *
@@ -61,6 +184,52 @@ YUI.add('ez-maplocation-editview', function (Y) {
             return {
                 "isRequired": this.get('fieldDefinition').isRequired
             };
+        }
+    }, {
+        ATTRS: {
+            /**
+             * The instance of the google map which is initialized in the widget
+             *
+             * @attribute map
+             * @type {Object}
+             * @default null
+             */
+            map: {
+                value: null
+            },
+
+            /**
+             * The location marker on the widget google map
+             *
+             * @attribute marker
+             * @type {Object}
+             * @default null
+             */
+            marker: {
+                value: null
+            },
+
+            /**
+             * Latitude of the current location
+             *
+             * @attribute latitude
+             * @type {Float}
+             * @default 0
+             */
+            latitude: {
+                value: 0
+            },
+
+            /**
+             * Longitude of the current location
+             *
+             * @attribute longitude
+             * @type {Float}
+             * @default 0
+             */
+            longitude: {
+                value: 0
+            }
         }
     });
 
