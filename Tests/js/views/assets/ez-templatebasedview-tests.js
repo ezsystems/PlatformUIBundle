0c53c14f030c0fc21f112f89a550e7b30eed949c
Damien Pobel <dp@ez.no>
2014-01-13 14:20:14 +0100

    Fixed template based view tests to be in an external file

---
+++ Tests/js/views/assets/ez-templatebasedview-tests.js
@@ -0,0 +1,44 @@
+YUI.add('ez-templatebasedview-tests', function (Y) {
+    var viewTest;
+
+    viewTest = new Y.Test.Case({
+        name: "eZ Template based view tests",
+
+        setUp: function () {
+            this.TestView = Y.Base.create('TestView', Y.eZ.TemplateBasedView, []);
+            this.TestViewNoTemplate = Y.Base.create('TestViewNoTemplate', Y.eZ.TemplateBasedView, []);
+        },
+
+        "Should find the template": function () {
+            var view = new this.TestView();
+
+            Y.Assert.isFunction(view.template, "The template property should be function");
+            Y.Assert.areEqual(
+                Y.one('#testview-ez-template').getHTML(), view.template(),
+                "The template should generate the content of the #testview-ez-template element"
+            );
+        },
+
+        "Should generate a template which returns an empty string is the template element is not found": function () {
+            var view = new this.TestViewNoTemplate();
+
+            Y.Assert.isFunction(view.template, "The template property should be function");
+            Y.Assert.areEqual(
+                "", view.template(),
+                "The template should generate the content of the #testview-ez-template element"
+            );
+        },
+
+        "containerTemplate property should include a class based on the view name": function () {
+            var view = new this.TestView();
+
+            Y.Assert.isTrue(
+                Y.Node.create(view.containerTemplate).hasClass("ez-view-testview"),
+                "A node created with containerTemplate property should have the class ez-view-testview"
+            );
+        }
+    });
+
+    Y.Test.Runner.setName("eZ Template Based view tests");
+    Y.Test.Runner.add(viewTest);
+}, '0.0.1', {requires: ['test', 'ez-templatebasedview']});
