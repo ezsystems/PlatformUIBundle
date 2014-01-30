7b91be7120a561e7191e27769f474c10221dc399
Damien Pobel <dp@ez.no>
2014-01-30 18:12:26 +0100

    Fixed EZP-22183: Implemented the basic behaviour of the navigation hub

--- Resources/public/js/views/ez-navigationhubview.js
+++ Resources/public/js/views/ez-navigationhubview.js
@@ -7,6 +7,15 @@ YUI.add('ez-navigationhubview', function (Y) {
      */
     Y.namespace('eZ');
 
+    var L = Y.Lang,
+        FIXED_NAVIGATION = 'is-navigation-fixed',
+        NAVIGATION_HIDDEN = 'is-navigation-hidden',
+        SUB_MENU_OPEN = 'is-sub-menu-open',
+        NAVIGATION_SEL = '.ez-navigation',
+        DEFAULT_ACTIVE_NAV = 'create',
+        ACTIVE_NAVIGATION_TPL = 'ez-navigation-{identifier}',
+        ZONE_ACTIVE = 'is-zone-active';
+
     /**
      * The navigation hub view
      *
@@ -16,9 +25,334 @@ YUI.add('ez-navigationhubview', function (Y) {
      * @extends eZ.TemplateBasedView
      */
     Y.eZ.NavigationHubView = Y.Base.create('navigationHubView', Y.eZ.TemplateBasedView, [], {
+        events: {
+            '.ez-zone': {
+                'mouseover': '_setNavigation',
+                'tap': '_setNavigation',
+            },
+            '.ez-sub-menu-link': {
+                'mouseover': '_uiShowSubMenu',
+                'mouseout': '_uiHideSubMenu',
+                'tap': '_uiToggleSubMenu',
+            }
+        },
+
+        /*
+         * The currently active navigation menu
+         *
+         * @property _navigationMenu
+         * @default null
+         * @type {Node}
+         */
+        _navigationMenu: null,
+
+        /**
+         * The y coordinate of the active menu when the navigation is not fixed.
+         *
+         * @property _navigationY
+         * @default 0
+         * @type Number
+         */
+        _navigationY: 0,
+
+        /**
+         * The scroll event subscription set to manage the fixed navigation
+         *
+         * @property
+         * @default null
+         * @type EventHandle
+         */
+        _scrollSubscription: null,
+
+        /**
+         * The resize event subscription set to manage the width of the
+         * navigation
+         *
+         * @property _resizeSubscription
+         * @default null
+         * @type EventHandle
+         */
+        _resizeSubscription: null,
+
+        initializer: function () {
+            this.after('activeNavigationChange', this._uiSetActiveNavigation);
+            this.after('navigationFixedChange', this._uiHandleFixedNavigation);
+            this.after('activeChange', this._onActiveUpdate);
+        },
+
+        /**
+         * Sets the active navigation in the UI
+         *
+         * @method _uiSetActiveNavigation
+         * @protected
+         */
+        _uiSetActiveNavigation: function () {
+            this._uiSetActiveZone();
+            this._uiShowNavigation();
+            this._uiNavigationSize();
+        },
+
+        /**
+         * Handles the fixed navigation in the UI
+         *
+         * @method _uiHandleFixedNavigation
+         * @protected
+         * @param {Object} e event facade
+         */
+        _uiHandleFixedNavigation: function (e) {
+            var container = this.get('container');
+
+            if ( e.newVal ) {
+                container.addClass(FIXED_NAVIGATION);
+            } else {
+                container.removeClass(FIXED_NAVIGATION);
+            }
+        },
+
+        /**
+         * Marks the zone associated with the activeNavigation as active
+         *
+         * @protected
+         * @method _uiSetActiveZone
+         */
+        _uiSetActiveZone: function () {
+            var container = this.get('container'),
+                active = container.one('.' + ZONE_ACTIVE);
+
+            if ( active ) {
+                active.removeClass(ZONE_ACTIVE);
+            }
+            container.one('.ez-' + this.get('activeNavigation') + '-zone').addClass(ZONE_ACTIVE);
+        },
+
+        /**
+         * Renders the navigation hub view
+         *
+         * @method render
+         * @return {eZ.NavigationHubView} the view itself
+         */
         render: function () {
-            this.get('container').setHTML(this.template());
+            var container = this.get('container');
+
+            container.setHTML(this.template());
+            this.set('activeNavigation', DEFAULT_ACTIVE_NAV); // TODO should depend on the app active view
             return this;
         },
+
+        _onActiveUpdate: function (e) {
+            var fixed = this.get('navigationFixed');
+
+            if ( e.newVal ) {
+                this._scrollSubscription = Y.on('scroll', Y.bind(this._handleScroll, this));
+                this._resizeSubscription = Y.on('resize', Y.bind(this._uiNavigationSize, this));
+                this.set('navigationFixed', false);
+                this._navigationY = this.get('container').one(NAVIGATION_SEL).getY();
+                this.set('navigationFixed', fixed);
+                this._uiNavigationSize();
+            } else {
+                this._scrollSubscription.detach();
+                this._resizeSubscription.detach();
+            }
+        },
+
+        /**
+         * Makes sure the navigation corresponding to the activeNavigation value
+         * is shown
+         *
+         * @protected
+         * @method _uiShowNavigation
+         */
+        _uiShowNavigation: function () {
+            var navigations = this.get('container').one(NAVIGATION_SEL).get('children'),
+                navClass = L.sub(ACTIVE_NAVIGATION_TPL, {identifier: this.get('activeNavigation')});
+
+            navigations.each(function (nav) {
+                if ( nav.hasClass(navClass) ) {
+                    nav.removeClass(NAVIGATION_HIDDEN);
+                } else {
+                    nav.addClass(NAVIGATION_HIDDEN);
+                }
+            });
+        },
+
+        /**
+         * tap/mouseover handler on the navigation zones. It sets the
+         * activeNavigation attribute to the corresponding value
+         *
+         * @protected
+         * @method _setNavigation
+         * @param {Object} e event facade
+         */
+        _setNavigation: function (e) {
+            this.set('activeNavigation', e.currentTarget.getAttribute('data-navigation'));
+        },
+
+        /**
+         * Handles the user scroll to make the navigation fixed or not
+         *
+         * @protected
+         * @method _handleScroll
+         * @param {Object} e event facade
+         */
+        _handleScroll: function (e) {
+            var navigation = this.get('container').one(NAVIGATION_SEL);
+
+            if ( navigation.get('docScrollY') > this._navigationY ) {
+                this.set('navigationFixed', true);
+                this.set('activeNavigation', DEFAULT_ACTIVE_NAV); // TODO should depend on the app activeView
+            } else {
+                this.set('navigationFixed', false);
+            }
+        },
+
+        /**
+         * Makes sure all the active navigation items are available in the menu.
+         * If there's too much items, the overflowed elements are put in the
+         * *more* sub menu
+         *
+         * @method _uiNavigationSize
+         * @protected
+         */
+        _uiNavigationSize: function (e) {
+            var items, item,
+                more = this._navigationMenu.one('.ez-more'),
+                moreMenu = more.one('.ez-sub-menu'),
+                moreItems = moreMenu.all('li');
+
+            while ( this._navigationUnderflowed() ) {
+                if ( moreItems.isEmpty() ) {
+                    this._navigationMenu.removeClass('has-more');
+                    break;
+                } else {
+                    item = moreItems.shift();
+                    this._navigationMenu.insert(item, more);
+                }
+            }
+            items = this._getNavigationItems();
+            while ( !items.isEmpty() && this._navigationOverflowed() ) {
+                this._navigationMenu.addClass('has-more');
+                item = items.pop();
+                moreMenu.prepend(item);
+            }
+        },
+
+        /**
+         * Checks whether there are too much items in the navigation for the
+         * current viewport size
+         *
+         * @protected
+         * @method _navigationOverflowed
+         * @return Boolean
+         */
+        _navigationOverflowed: function () {
+            return this._navigationMenu.get('scrollWidth') > this._navigationMenu.get('docWidth');
+        },
+
+        /**
+         * Checks whether the navigation is not completely filled up
+         *
+         * @protected
+         * @method _navigationUnderflowed
+         * @return Boolean
+         */
+        _navigationUnderflowed: function () {
+            return this._navigationMenu.get('scrollWidth') <= this._navigationMenu.get('docWidth');
+        },
+
+        /**
+         * Returns all navigation items except the *More* item
+         *
+         * @method _getNavigationItems
+         * @protected
+         * @return NodeList
+         */
+        _getNavigationItems: function () {
+            return this._navigationMenu.all('> li:not(.ez-more)');
+        },
+
+        /**
+         * mouseover event handler for the sub menu links to show and place
+         * the corresponding sub menu
+         *
+         * @method _uiShowSubMenu
+         * @protected
+         * @param {Object} e event facade
+         */
+        _uiShowSubMenu: function (e) {
+            var link = e.currentTarget,
+                subMenu = link.one('.ez-sub-menu');
+
+            link.addClass(SUB_MENU_OPEN);
+            subMenu.setStyle('min-width', link.get('offsetWidth') + 'px');
+            subMenu.setXY([
+                link.getX(),
+                link.getY() + link.get('offsetHeight')
+            ]);
+            // make sure the menu is fully in the view port
+            if ( !subMenu.inViewportRegion(Y.config.doc) ) {
+                subMenu.setX(subMenu.get('winWidth') - subMenu.get('offsetWidth'));
+            }
+            this._clickOutsideSubscription = link.on('clickoutside', Y.bind(this._uiHideSubMenu, this));
+        },
+
+        /**
+         * mouseout/clickoutside event handler for the sub menu links to hide
+         * the corresponding sub menu
+         *
+         * @method _uiHideSubMenu
+         * @protected
+         * @param {Object} e event facade
+         */
+        _uiHideSubMenu: function (e) {
+            e.currentTarget.removeClass(SUB_MENU_OPEN);
+            this._clickOutsideSubscription.detach();
+        },
+
+        /**
+         * Toggles the sub menu visibility
+         *
+         * @protected
+         * @method _uiToggleSubMenu
+         * @param {Object} e event facade
+         */
+        _uiToggleSubMenu: function (e) {
+            if ( e.currentTarget.hasClass(SUB_MENU_OPEN) ) {
+                this._uiHideSubMenu(e);
+            } else {
+                this._uiShowSubMenu(e);
+            }
+        },
+    }, {
+        ATTRS: {
+            /**
+             * Contains the identifier ('create', 'optimize', ...) of the
+             * currently active navigation. When set, this attribute updates the
+             * `_navigationMenu` property with the corresponding Node
+             *
+             * @attribute activeNavigation
+             * @type String
+             * @default null
+             * @required
+             */
+            activeNavigation: {
+                setter: function (val, name) {
+                    this._navigationMenu = this.get('container').one(
+                        '.' + L.sub(ACTIVE_NAVIGATION_TPL, {identifier: val})
+                    );
+                    return val;
+                }
+            },
+
+            /**
+             * Whether the navigation is in fixed more or not
+             *
+             * @attribute navigationFixed
+             * @type Boolean
+             * @default false
+             */
+            navigationFixed: {
+                value: false
+            }
+        }
     });
 });
---
+++ Tests/js/views/assets/ez-navigationhubview-tests.js
@@ -0,0 +1,343 @@
+YUI.add('ez-navigationhubview-tests', function (Y) {
+    var viewTest, eventTest;
+
+    viewTest = new Y.Test.Case({
+        name: "eZ Navigation Hub View test",
+
+        setUp: function () {
+            this.view = new Y.eZ.NavigationHubView({
+                container: '.container',
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+        },
+
+        "Test render": function () {
+            var templateCalled = false,
+                origTpl;
+
+            origTpl = this.view.template;
+            this.view.template = function () {
+                templateCalled = true;
+                return origTpl.apply(this, arguments);
+            };
+            this.view.render();
+            Y.Assert.isTrue(templateCalled, "The template should have used to render the this.view");
+        },
+
+        "Test available variable in template": function () {
+            var origTpl = this.view.template;
+
+            this.view.template = function (variables) {
+                Y.Assert.isUndefined(variables, "The template should not receive any variable");
+
+                return origTpl.apply(this, variables);
+            };
+            this.view.render();
+        },
+    });
+
+    eventTest = new Y.Test.Case({
+        name: "eZ Navigation Hub View event tests",
+
+        setUp: function () {
+            this.view = new Y.eZ.NavigationHubView({
+                container: '.container',
+            });
+            this.view.render();
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+        },
+
+        _testShowNavigationMenu: function (zoneNode, navigationIdentifier) {
+            Y.Assert.isTrue(
+                zoneNode.hasClass("is-zone-active"),
+                "The choosen zone should be active"
+            );
+
+            this.view.get('container').one('.ez-navigation').get('children').each(function () {
+                if ( this.getAttribute('class').indexOf(navigationIdentifier) === -1 ) {
+                    Y.Assert.isTrue(
+                        this.hasClass('is-navigation-hidden'),
+                        "The non active navigation menu should be hidden"
+                    );
+                } else {
+                    Y.Assert.isFalse(
+                        this.hasClass('is-navigation-hidden'),
+                        "The active navigation menu should be showed"
+                    );
+                    Y.Assert.isTrue(
+                        this.hasClass('has-more'),
+                        "The 'More' link is visible in the active navigation menu"
+                    );
+                    Y.Assert.areNotEqual(
+                        0, this.one('.ez-more .ez-sub-menu').get('children').size(),
+                        "The 'more' sub menu be filled with some items"
+                    );
+                }
+            });
+        },
+
+        "Should show the correct navigation menu when the mouse is over a navigation zone": function () {
+            var container = this.view.get('container'),
+                optZone = container.one('.ez-optimize-zone'),
+                navigationIdentifier = optZone.getAttribute('data-navigation');
+
+            optZone.simulate('mouseover');
+            this._testShowNavigationMenu(optZone, navigationIdentifier);
+        },
+
+        "Should show the correct navigation menu when tapping a navigation zone": function () {
+            var container = this.view.get('container'),
+                optZone = container.one('.ez-optimize-zone'),
+                navigationIdentifier = optZone.getAttribute('data-navigation'),
+                that = this;
+
+            optZone.simulateGesture('tap', function () {
+                that.resume(function () {
+                    this._testShowNavigationMenu(optZone, navigationIdentifier);
+                });
+            });
+            this.wait();
+        },
+
+        _testShowSubMenu: function (link, subMenu, testCoordinates) {
+            Y.Assert.isTrue(
+                link.hasClass('is-sub-menu-open'),
+                "The sub menu link should get the is-sub-menu-open class"
+            );
+            Y.Assert.areEqual(
+                link.get('offsetWidth') + 'px',
+                subMenu.getStyle('min-width'),
+                "The sub menu should have the width of link as min width"
+            );
+            if ( testCoordinates ) {
+                Y.Assert.areEqual(
+                    link.getX(), subMenu.getX(),
+                    "The sub menu should be aligned with the link on the X coordinate"
+                );
+                Y.Assert.areEqual(
+                    link.getY() + link.get('offsetHeight'), subMenu.getY(),
+                    "The sub menu should be aligned with the bottom of the link on the Y coordinate"
+                );
+            } else {
+                Y.Assert.isTrue(
+                    subMenu.inViewportRegion(Y.config.doc),
+                    "The sub menu should be in the viewport"
+                );
+            }
+        },
+
+        "Should show the sub-menu when the mouse is over its sub menu link": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu');
+
+            subMenuLink.simulate('mouseover');
+            this._testShowSubMenu(subMenuLink, subMenu, true);
+        },
+
+        "Should show the sub-menu when its sub menu link is tapped": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu'),
+                that = this;
+
+            subMenuLink.simulateGesture('tap', function () {
+                that.resume(function () {
+                    this._testShowSubMenu(subMenuLink, subMenu, true);
+                });
+            });
+            this.wait();
+        },
+
+        "Should show the more sub-menu when the mouse is over its sub menu link": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-more.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu');
+
+            subMenuLink.simulate('mouseover');
+            this._testShowSubMenu(subMenuLink, subMenu, false);
+        },
+
+        "Should show the more sub-menu when its sub menu link is tapped": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-more.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu'),
+                that = this;
+
+            subMenuLink.simulateGesture('tap', function () {
+                that.resume(function () {
+                    this._testShowSubMenu(subMenuLink, subMenu, false);
+                });
+            });
+            this.wait();
+        },
+
+        _testHiddenSubMenu: function (submenu) {
+            Y.Assert.isFalse(
+                submenu.hasClass('is-sub-menu-open'),
+                "The sub menu should be closed"
+            );
+        },
+
+        "Should hide the sub menu when the mouse is out of its sub menu link": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-more.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu');
+
+            subMenuLink.simulate('mouseover');
+            subMenuLink.simulate('mouseout');
+            this._testHiddenSubMenu(subMenu);
+        },
+
+        "Should hide the sub menu when tapping one of its item": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-more.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu'),
+                that = this;
+
+            subMenuLink.simulateGesture('tap', function () {
+                that.resume(function () {
+                    subMenu.one('.ez-navigation-item').simulateGesture('tap', function () {
+                        that.resume(function () {
+                            this._testHiddenSubMenu(subMenu);
+                        });
+                    });
+                    this.wait();
+                });
+            });
+            this.wait();
+        },
+
+        "Should hide the sub menu when clicking outside": function () {
+            var container = this.view.get('container'),
+                subMenuLink = container.one('.ez-more.ez-sub-menu-link'),
+                subMenu = subMenuLink.one('.ez-sub-menu'),
+                that = this;
+
+            subMenuLink.simulateGesture('tap', function () {
+                that.resume(function () {
+                    container.simulate('click');
+                    this._testHiddenSubMenu(subMenu);
+                });
+            });
+            this.wait();
+        },
+
+        _scrollTo: function (amount) {
+            var container = this.view.get('container');
+
+            container.setStyle('margin-top', amount);
+            this.view.set('active', false);
+            this.view.set('active', true);
+            container.simulate('scroll');
+        },
+
+        "Should set the navigationFixed attribute depending on scroll": function () {
+            this._scrollTo('-300px');
+            Y.Assert.isTrue(
+                this.view.get('navigationFixed'),
+                "The navigation fixed attribute should be true"
+            );
+
+            this._scrollTo('100px');
+            Y.Assert.isFalse(
+                this.view.get('navigationFixed'),
+                "The navigation fixed attribute should be false"
+            );
+            this._scrollTo(0);
+        },
+
+        "Should fix/unfix the navigation menu": function () {
+            this.view.set('navigationFixed', true);
+            Y.Assert.isTrue(
+                this.view.get('container').hasClass('is-navigation-fixed'),
+                "The view container should get the 'is-navigation-fixed' class"
+            );
+            this.view.set('navigationFixed', false);
+            Y.Assert.isFalse(
+                this.view.get('container').hasClass('is-navigation-fixed'),
+                "The view container should not have the 'is-navigation-fixed' class"
+            );
+        },
+
+        "Should put some elements in the more sub menu": function () {
+            var container = this.view.get('container'),
+                navigationMenu = container.one('.ez-navigation-create'),
+                more = navigationMenu.one('.ez-more'),
+                inMoreMenu = [], inMoreMenuAfter = [];
+
+            this.view.set('active', true);
+            this.view.set('activeNavigation', 'create');
+
+            // emptying the more navigation
+            navigationMenu.all('.ez-more li').each(function () {
+                inMoreMenu.push(this);
+                navigationMenu.insert(this, more);
+            });
+            navigationMenu.removeClass('has-more');
+
+            container.simulate('resize');
+
+            Y.Assert.isTrue(
+                navigationMenu.hasClass('has-more'),
+                "The 'More' link should be visible in the active navigation menu"
+            );
+            Y.Assert.areEqual(
+                inMoreMenu.length,
+                navigationMenu.one('.ez-more .ez-sub-menu').get('children').size(),
+                "The 'more' sub menu be filled with some numbers of items as initially"
+            );
+
+            navigationMenu.all('.ez-more li').each(function () {
+                inMoreMenuAfter.push(this);
+            });
+
+            Y.Array.each(inMoreMenu, function (val, key) {
+                Y.Assert.areSame(
+                    val, inMoreMenuAfter[key],
+                    "The more menu should remain the same"
+                );
+            });
+        },
+
+        "Should take some elements from the more sub menu": function () {
+            var container = this.view.get('container'),
+                navigationMenu = container.one('.ez-navigation-create');
+
+            this.view.set('activeNavigation', 'create');
+            this.view.set('active', true);
+
+            // making some space in the menu
+            navigationMenu.all('li').pop().remove();
+            navigationMenu.all('li').pop().remove();
+            // leaving only one element in the more menu
+            navigationMenu.all('.ez-more li:not(.last)').each(function () {
+                this.remove();
+            });
+            navigationMenu.removeClass('has-more');
+
+            container.simulate('resize');
+
+            Y.Assert.isFalse(
+                navigationMenu.hasClass('has-more'),
+                "The 'More' link should not be visible in the active navigation menu"
+            );
+
+            Y.Assert.areEqual(
+                0, navigationMenu.all('.ez-more li').size(),
+                "The 'More' menu should be empty"
+            );
+        },
+    });
+
+    Y.Test.Runner.setName("eZ Navigation Hub View tests");
+    Y.Test.Runner.add(viewTest);
+    Y.Test.Runner.add(eventTest);
+
+}, '0.0.1', {requires: ['test', 'node-event-simulate', 'ez-navigationhubview']});
