18df4ed43408ecf71b536e3ef6b539a742b7a1a8
Jérôme Vieilledent <lolautruche@gmail.com>
2014-07-24 14:15:31 +0200

    EZP-23014: Loaded Assetic configuration from within the bundle
    Prevents configuration at application level.

--- DependencyInjection/EzPlatformUIExtension.php
+++ DependencyInjection/EzPlatformUIExtension.php
@@ -9,13 +9,15 @@
 namespace EzSystems\PlatformUIBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Container;
+use EzSystems\PlatformUIBundle\EzSystemsPlatformUIBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class EzPlatformUIExtension extends Extension
+class EzPlatformUIExtension extends Extension implements PrependExtensionInterface
 {
     public function getAlias()
     {
@@ -33,5 +35,9 @@ class EzPlatformUIExtension extends Extension
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
         $loader->load( 'services.yml' );
         $loader->load( 'yui.yml' );
+
+    public function prepend( ContainerBuilder $container )
+    {
+        $container->prependExtensionConfig( 'assetic', array( 'bundles' => array( EzSystemsPlatformUIBundle::NAME ) ) );
     }
 }
