1bc36866c6dbc9e1bc81329dd89c8c6f5bd298f3
Damien Pobel <dp@ez.no>
2014-10-31 12:20:34 +0100

    EZP-23432: added a unit test for the case where the publish draft creates the content

--- Tests/js/views/services/plugins/assets/ez-publishdraftplugin-tests.js
+++ Tests/js/views/services/plugins/assets/ez-publishdraftplugin-tests.js
@@ -11,17 +11,79 @@ YUI.add('ez-publishdraftplugin-tests', function (Y) {
 
         setUp: function () {
             this.publishRedirectionUrl = '/something';
+            this.languageCode = 'eng-GB';
             this.app = new Y.Mock();
             this.capi = {};
             this.version = new Y.Mock();
             this.content = new Y.Mock();
+            this.contentType = {};
+            this.parentLocation = {};
+            this.createContentResponse = {
+                "Content": {
+                    "CurrentVersion": {
+                        "Version": {
+                            "_media-type": "application/vnd.ez.api.Version+json",
+                            "_href": "/api/ezp/v2/content/objects/4242/versions/3",
+                            "VersionInfo": {
+                                "id": "42",
+                                "versionNo": 3,
+                                "status": "DRAFT",
+                                "modificationDate": "2014-02-25T14:12:04+01:00",
+                                "Creator": {
+                                    "_media-type": "application/vnd.ez.api.User+json",
+                                    "_href": "/api/ezp/v2/user/users/14"
+                                },
+                                "creationDate": "2014-02-25T14:12:04+01:00",
+                                "initialLanguageCode": "eng-GB",
+                                "languageCodes": "eng-GB",
+                                "names": {
+                                    "value": [
+                                        {
+                                            "_languageCode": "eng-GB",
+                                            "#text": "T11"
+                                        }
+                                    ]
+                                },
+                                "Content": {
+                                    "_media-type": "application/vnd.ez.api.ContentInfo+json",
+                                    "_href": "/api/ezp/v2/content/objects/4242"
+                                }
+                            },
+                            "Fields": {
+                                "field": [
+                                    {
+                                        "id": 978,
+                                        "fieldDefinitionIdentifier": "name",
+                                        "languageCode": "eng-GB",
+                                        "fieldValue": "T11"
+                                    },
+                                    {
+                                        "id": 979,
+                                        "fieldDefinitionIdentifier": "text",
+                                        "languageCode": "eng-GB",
+                                        "fieldValue": "Once and for all"
+                                    }
+                                ]
+                            },
+                            "Relations": {
+                                "_media-type": "application/vnd.ez.api.RelationList+json",
+                                "_href": "/api/ezp/v2/content/objects/4242/versiaons/3/relations",
+                                "Relation": []
+                            }
+                        }
+                    }
+                }
+            };
 
             this.service = new Y.Base();
             this.service.set('app', this.app);
             this.service.set('capi', this.capi);
             this.service.set('publishRedirectionUrl', this.publishRedirectionUrl);
+            this.service.set('languageCode', this.languageCode);
             this.service.set('version', this.version);
             this.service.set('content', this.content);
+            this.service.set('contentType', this.contentType);
+            this.service.set('parentLocation', this.parentLocation);
 
             this.view = new Y.View();
             this.view.addTarget(this.service);
@@ -42,6 +104,8 @@ YUI.add('ez-publishdraftplugin-tests', function (Y) {
             delete this.app;
             delete this.version;
             delete this.content;
+            delete this.parentLocation;
+            delete this.contentType;
         },
 
         "Should publish the draft": function () {
@@ -89,6 +153,11 @@ YUI.add('ez-publishdraftplugin-tests', function (Y) {
                         "The publish option should be set true"
                     );
                     Assert.areEqual(contentId, options.contentId, "The content id should be passed");
+                    Assert.areSame(
+                        that.languageCode,
+                        options.languageCode,
+                        "The save options should contain the language code"
+                    );
                     callback();
                 }
             });
@@ -111,23 +180,121 @@ YUI.add('ez-publishdraftplugin-tests', function (Y) {
             Y.Mock.verify(this.app);
         },
 
-        "Should not publish the draft": function () {
-            Y.Mock.expect(this.version, {
-                method: 'save',
-                args: [Y.Mock.Value.Object, Y.Mock.Value.Function],
-                run: function () {
-                    Assert.fail("The version should not be saved");
-                }
+        "Should not do anything": function () {
+            Y.Mock.expect(this.app, {
+                method: 'set',
+                callCount: 0
             });
             this.view.fire('whatever:publishAction', {
                 formIsValid: false
             });
+            Y.Mock.verify(this.app);
+        },
+
+        "Should create the content and publish it": function () {
+            var that = this,
+                fields = [],
+                response = {document: this.createContentResponse},
+                versionAttrs = {};
+
+            Y.Mock.expect(this.content, {
+                method: 'isNew',
+                returns: true,
+            });
+            Y.Mock.expect(this.content, {
+                method: 'save',
+                args: [Y.Mock.Value.Object, Y.Mock.Value.Function],
+                run: function (options, callback) {
+                    Assert.areSame(
+                        that.capi,
+                        options.api,
+                        "The CAPI should be passed to save"
+                    );
+                    Assert.areEqual(
+                        that.languageCode,
+                        options.languageCode,
+                        "The language code should be passed to save"
+                    );
+                    Assert.areEqual(
+                        that.contentType,
+                        options.contentType,
+                        "The content type should be passed to save"
+                    );
+                    Assert.areEqual(
+                        that.parentLocation,
+                        options.parentLocation,
+                        "The parent location should be passed to save"
+                    );
+                    Assert.areEqual(
+                        fields,
+                        options.fields,
+                        "The fields should be passed to save"
+                    );
+                    callback(false, response);
+                }
+            });
+            Y.Mock.expect(this.content, {
+                method: 'load',
+                args: [Y.Mock.Value.Object, Y.Mock.Value.Function],
+                run: function (options, callback) {
+                    Assert.areSame(
+                        that.capi,
+                        options.api,
+                        "The CAPI should be passed to load"
+                    );
+                    callback();
+                }
+            });
+            Y.Mock.expect(this.version, {
+                method: 'parse',
+                args: [Y.Mock.Value.Object],
+                run: function (doc) {
+                    Assert.areSame(
+                        that.createContentResponse.Content.CurrentVersion,
+                        doc.document,
+                        "parse should have received a version response"
+                    );
+                    return versionAttrs;
+                }
+            });
+            Y.Mock.expect(this.version, {
+                method: 'setAttrs',
+                args: [versionAttrs],
+            });
+            Y.Mock.expect(this.version, {
+                method: 'publishVersion',
+                args: [Y.Mock.Value.Object, Y.Mock.Value.Function],
+                run: function (options, callback) {
+                    Assert.areSame(
+                        that.capi,
+                        options.api,
+                        "The CAPI should be passed to publishVersion"
+                    );
+                    callback();
+                }
+            });
+            Y.Mock.expect(this.app, {
+                method: 'set',
+                args: ['loading', true]
+            });
+            Y.Mock.expect(this.app, {
+                method: 'navigate',
+                args: [this.publishRedirectionUrl],
+            });
+            this.view.fire('whatever:publishAction', {
+                formIsValid: true,
+                fields: fields
+            });
+
+            Y.Mock.verify(this.content);
+            Y.Mock.verify(this.version);
+            Y.Mock.verify(this.app);
         },
     });
 
     registerTest = new Y.Test.Case(Y.eZ.Test.PluginRegisterTest);
     registerTest.Plugin = Y.eZ.Plugin.PublishDraft;
-    registerTest.components = ['contentEditViewService'];
+    registerTest.components = ['contentEditViewService', 'contentCreateViewService'];
 
     Y.Test.Runner.setName("eZ Publish Draft Plugin tests");
     Y.Test.Runner.add(tests);
