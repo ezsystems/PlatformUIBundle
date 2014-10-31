1d82578be1352e7344ebb296da595d7b5208d03b
Damien Pobel <dp@ez.no>
2014-10-31 15:10:35 +0100

    EZP-23432: added a unit tests for the case where the save draft creates the content

--- Tests/js/views/services/plugins/assets/ez-savedraftplugin-tests.js
+++ Tests/js/views/services/plugins/assets/ez-savedraftplugin-tests.js
@@ -13,11 +13,17 @@ YUI.add('ez-savedraftplugin-tests', function (Y) {
             this.capi = {};
             this.version = new Y.Mock();
             this.content = new Y.Mock();
+            this.contentType = {};
+            this.parentLocation = {};
+            this.languageCode = 'fre-FR';
 
             this.service = new Y.Base();
             this.service.set('capi', this.capi);
             this.service.set('version', this.version);
             this.service.set('content', this.content);
+            this.service.set('languageCode', this.languageCode);
+            this.service.set('contentType', this.contentType);
+            this.service.set('parentLocation', this.parentLocation);
 
             this.view = new Y.View();
             this.view.addTarget(this.service);
@@ -25,6 +31,63 @@ YUI.add('ez-savedraftplugin-tests', function (Y) {
             this.plugin = new Y.eZ.Plugin.SaveDraft({
                 host: this.service,
             });
+
+            this.createContentResponse = {
+                "Content": {
+                    "CurrentVersion": {
+                        "Version": {
+                            "_media-type": "application/vnd.ez.api.Version+json",
+                            "_href": "/api/ezp/v2/content/objects/4242/versions/3",
+                            "VersionInfo": {
+                                "id": "42",
+                                "versionNo": 3,
+                                "status": "DRAFT",
+                                "modificationDate": "2014-02-25T14:12:04+01:00",
+                                "Creator": {
+                                    "_media-type": "application/vnd.ez.api.User+json",
+                                    "_href": "/api/ezp/v2/user/users/14"
+                                },
+                                "creationDate": "2014-02-25T14:12:04+01:00",
+                                "initialLanguageCode": "eng-GB",
+                                "languageCodes": "eng-GB",
+                                "names": {
+                                    "value": [
+                                        {
+                                            "_languageCode": "eng-GB",
+                                            "#text": "T11"
+                                        }
+                                    ]
+                                },
+                                "Content": {
+                                    "_media-type": "application/vnd.ez.api.ContentInfo+json",
+                                    "_href": "/api/ezp/v2/content/objects/4242"
+                                }
+                            },
+                            "Fields": {
+                                "field": [
+                                    {
+                                        "id": 978,
+                                        "fieldDefinitionIdentifier": "name",
+                                        "languageCode": "eng-GB",
+                                        "fieldValue": "T11"
+                                    },
+                                    {
+                                        "id": 979,
+                                        "fieldDefinitionIdentifier": "text",
+                                        "languageCode": "eng-GB",
+                                        "fieldValue": "Once and for all"
+                                    }
+                                ]
+                            },
+                            "Relations": {
+                                "_media-type": "application/vnd.ez.api.RelationList+json",
+                                "_href": "/api/ezp/v2/content/objects/4242/versiaons/3/relations",
+                                "Relation": []
+                            }
+                        }
+                    }
+                }
+            };
         },
 
         tearDown: function () {
@@ -37,6 +100,7 @@ YUI.add('ez-savedraftplugin-tests', function (Y) {
             delete this.service;
             delete this.content;
             delete this.version;
+            delete this.contentType;
         },
 
         "Should save the draft": function () {
@@ -68,6 +132,11 @@ YUI.add('ez-savedraftplugin-tests', function (Y) {
                         "The fields from the event facade should be passed in the save options"
                     );
                     Assert.areEqual(contentId, options.contentId, "The content id should be passed");
+                    Assert.areSame(
+                        that.languageCode,
+                        options.languageCode,
+                        "The save options should contain the language code"
+                    );
                     callback();
                 }
             });
@@ -77,6 +146,73 @@ YUI.add('ez-savedraftplugin-tests', function (Y) {
                 fields: fields
             });
 
+            Y.Mock.verify(this.version);
+        },
+
+        "Should create the content as a draft": function () {
+            var fields = [{}, {}],
+                attrs = {},
+                that = this;
+
+            Y.Mock.expect(this.content, {
+                method: 'isNew',
+                returns: true,
+            });
+            Y.Mock.expect(this.content, {
+                method: 'save',
+                args: [Y.Mock.Value.Object, Y.Mock.Value.Function],
+                run: function (options, callback) {
+                    Assert.areSame(
+                        that.capi,
+                        options.api,
+                        "The save options should contain the CAPI"
+                    );
+                    Assert.areSame(
+                        fields,
+                        options.fields,
+                        "The fields from the event facade should be passed in the save options"
+                    );
+                    Assert.areSame(
+                        that.contentType,
+                        options.contentType,
+                        "The save options should contain the content type"
+                    );
+                    Assert.areSame(
+                        that.parentLocation,
+                        options.parentLocation,
+                        "The save options should contain the parent location"
+                    );
+                    Assert.areSame(
+                        that.languageCode,
+                        options.languageCode,
+                        "The save options should contain the language code"
+                    );
+                    callback(undefined, {document: that.createContentResponse});
+                }
+            });
+            Y.Mock.expect(this.version, {
+                method: "parse",
+                args: [Y.Mock.Value.Object],
+                run: function (response) {
+                    Assert.areSame(
+                        that.createContentResponse.Content.CurrentVersion,
+                        response.document,
+                        "parse should have received a version response"
+                    );
+                    return attrs;
+                }
+            });
+            Y.Mock.expect(this.version, {
+                method: "setAttrs",
+                args: [attrs]
+            });
+
+            this.view.fire('whatever:saveAction', {
+                formIsValid: true,
+                fields: fields
+            });
+
+            Y.Mock.verify(this.content);
             Y.Mock.verify(this.version);
         },
 
@@ -96,7 +232,7 @@ YUI.add('ez-savedraftplugin-tests', function (Y) {
 
     registerTest = new Y.Test.Case(Y.eZ.Test.PluginRegisterTest);
     registerTest.Plugin = Y.eZ.Plugin.SaveDraft;
-    registerTest.components = ['contentEditViewService'];
+    registerTest.components = ['contentEditViewService', 'contentCreateViewService'];
 
     Y.Test.Runner.setName("eZ Save Draft Plugin tests");
     Y.Test.Runner.add(tests);
