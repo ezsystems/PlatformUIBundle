89650e5eca1cebc5e8bf2e621747479c1ff44087
Damien Pobel <dp@ez.no>
2014-11-24 18:07:55 +0100

    EZP-23536: Changed the SelectionFilterView to accept an array of object as a source

--- Tests/js/views/assets/ez-selectionfilterview-tests.js
+++ Tests/js/views/assets/ez-selectionfilterview-tests.js
@@ -5,6 +5,7 @@
 YUI.add('ez-selectionfilterview-tests', function (Y) {
     var viewTest, filterDisabledTest, preselectedTest,
         multipleSelectTest, multipleUnselectTest, sourceValidationTest,
+        objectSourceTest, sourceChangeTest,
         source = ['Iron Man', 'Hulk', 'Captain America', 'Thor', 'Black Widow', 'Hawkeye', 'Loki', 'Nick Fury'];
 
     viewTest = new Y.Test.Case({
@@ -517,6 +518,325 @@ YUI.add('ez-selectionfilterview-tests', function (Y) {
         },
     });
 
+    objectSourceTest = new Y.Test.Case({
+        name: "eZ Selection Filter view source validation tests",
+
+        setUp: function () {
+            var that = this, i = 0;
+
+            this.source = [];
+            Y.Array.each(source, function (element) {
+                that.source.push({
+                    text: element,
+                    length: element.length,
+                    id: i++,
+                });
+            });
+            this.initialDOM = '<input type="text" id="input-selection-filter" class=""><ul class=""></ul>';
+            Y.one('.container').setHTML(this.initialDOM);
+            Y.config.doc.documentElement.focus();
+
+            this._resultTextLocator = function (elt) {
+                return elt.text + "(length: " + elt.length + ")";
+            };
+            this._resultAttributesFormatter = function (elt) {
+                return {
+                    text: elt.text,
+                    length: elt.length,
+                    position: elt.id + 1,
+                };
+            };
+            this.view = new Y.eZ.SelectionFilterView({
+                container: '.container',
+                inputNode: '.container input',
+                listNode: '.container ul',
+                source: this.source,
+                resultFilters: 'startsWith',
+                resultHighlighter: 'startsWith',
+                resultAttributesFormatter: this._resultAttributesFormatter,
+                resultTextLocator: this._resultTextLocator,
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+            delete this.view;
+        },
+
+        _assertDefaultList: function () {
+            var items = this.view.get('listNode').all(this.view.ITEM_TAG);
+
+            Y.Assert.areEqual(
+                source.length, items.size(),
+                "The list should contain as many items as in the source"
+            );
+
+            Y.Array.each(this.source, function (val) {
+                var item = this.view.get('listNode').one('[data-text="' + val.text + '"]'),
+                    expectedAttrs = this._resultAttributesFormatter(val);
+
+                Y.Assert.isObject(item, "The item for '" + val + "' is missing");
+                Y.Assert.areEqual(
+                    this._resultTextLocator(val), item.getContent(),
+                    "The visible value should be the result of the resultTextLocator function"
+                );
+                Y.Object.each(expectedAttrs, function (attrValue, key) {
+                    Y.Assert.areEqual(
+                        attrValue, item.getAttribute('data-' + key),
+                        "The item should have a data-" + key + " attribute with the value " + attrValue
+                    );
+                });
+            }, this);
+        },
+
+        "render should display the list of options": function () {
+            this.view.render();
+            this._assertDefaultList();
+        },
+
+        "destroy() test": function () {
+            this.view.render();
+            this.view.destroy();
+
+            Y.Assert.areEqual(
+                this.initialDOM, this.view.get('container').get('innerHTML'),
+                "The DOM should be restored to its initial state after destroy"
+            );
+        },
+
+        "focus() test": function () {
+            this.view.render();
+
+            this.view.focus();
+            Y.Assert.areSame(
+                Y.config.doc.activeElement, this.view.get('inputNode').getDOMNode(),
+                "The input node should get the focus"
+            );
+        },
+
+        "resetFilter() test": function () {
+            var inputNode = this.view.get('inputNode'),
+                that = this;
+
+            this.view.render();
+            this.view.after('results', function () {
+                that.resume(function () {
+                    this.view.resetFilter();
+
+                    Y.Assert.areEqual(
+                        "", inputNode.get('value'),
+                        "The filter input should be empty"
+                    );
+                    this._assertDefaultList();
+                });
+            });
+            inputNode.simulate('mousedown');
+            inputNode.set('value', 'Hulk');
+            this.wait();
+        },
+
+        "clear event handler should restore the default list": function () {
+            var inputNode = this.view.get('inputNode'),
+                that = this;
+
+            this.view.render();
+            this.view.after('results', function () {
+                that.resume(function () {
+                    this.view.after('clear', function () {
+                        that._assertDefaultList();
+                    });
+                    this.view.fire('clear');
+                });
+            });
+            inputNode.simulate('mousedown');
+            inputNode.set('value', 'Hulk');
+            this.wait();
+        },
+
+        "mouseover/mouseout an item": function () {
+            var listNode = this.view.get('listNode'), item;
+
+            this.view.render();
+
+            item = listNode.one('li[data-text=Loki]');
+            item.simulate('mouseover');
+            Y.Assert.isTrue(
+                item.hasClass('ez-selection-filter-item-active'),
+                "The item should have the active class"
+            );
+            item.simulate('mouseout');
+            Y.Assert.isFalse(
+                item.hasClass('ez-selection-filter-item-active'),
+                "The item should not have the active class"
+            );
+        },
+
+        "select an item": function () {
+            var listNode = this.view.get('listNode'), item,
+                choice = 'Black Widow',
+                selectEvt = false,
+                that = this;
+
+            this.view.render();
+
+            item = listNode.one('li[data-text="' + choice + '"]');
+            this.view.on('select', function (e) {
+                selectEvt = true;
+
+                Y.Assert.areEqual(
+                    choice, e.text,
+                    "The event facade should contain the selected value"
+                );
+                Y.Assert.areSame(
+                    item, e.elementNode,
+                    "The event facade should contain the selected item node"
+                );
+            });
+
+            item.simulateGesture('tap', function () {
+                that.resume(function () {
+                    Y.Assert.areEqual(
+                        1, this.view.get('selected').length,
+                        "The selected attribute should contain one choice"
+                    );
+                    Y.Assert.areEqual(
+                        choice, this.view.get('selected')[0],
+                        "The selected attribute should contain '" + choice + "'"
+                    );
+                    Y.Assert.isTrue(
+                        item.hasClass('ez-selection-filter-item-selected'),
+                        "The item should have the selected class"
+                    );
+
+                    Y.Assert.isTrue(selectEvt, "The select event should have been fired");
+
+                });
+            });
+            this.wait();
+        },
+
+        "select another item": function () {
+            var listNode = this.view.get('listNode'), item, item2,
+                choice = this.source[4],
+                choice2 = this.source[0],
+                selectEvt = false,
+                that = this;
+
+            this.view.render();
+
+            item = listNode.one('li[data-text="' + choice.text + '"]');
+            item2 = listNode.one('li[data-text="' + choice2.text + '"]');
+
+            item.simulateGesture('tap', function () {
+                that.resume(function () {
+                    this.view.on('select', function (e) {
+                        var expectedAttributes = that._resultAttributesFormatter(choice2);
+
+                        selectEvt = true;
+
+                        Y.Assert.areEqual(
+                            choice2.text, e.text,
+                            "The event facade should contain the selected value"
+                        );
+                        Y.Assert.areSame(
+                            item2, e.elementNode,
+                            "The event facade should contain the selected item node"
+                        );
+                        Y.Assert.isObject(
+                            e.attributes,
+                            "The event facade should provide the attributes of the selected element"
+                        );
+                        Y.Assert.areEqual(
+                            Y.Object.keys(expectedAttributes).length,
+                            Y.Object.keys(e.attributes).length,
+                            "The event facade should contain the attributes returned by the attributes formater"
+                        );
+                        Y.Object.each(e.attributes, function (value, key) {
+                            Y.Assert.areEqual(
+                                expectedAttributes[key], value,
+                                "The attribute " + key + " should have the value " + value
+                            );
+                        });
+                    });
+
+                    item2.simulateGesture('tap', function () {
+                        that.resume(function () {
+                            Y.Assert.areEqual(
+                                1, this.view.get('selected').length,
+                                "The selected attribute should contain one choice"
+                            );
+                            Y.Assert.areEqual(
+                                choice2.text, this.view.get('selected')[0],
+                                "The selected attribute should contain '" + choice2 + "'"
+                            );
+
+                            Y.Assert.isTrue(
+                                item2.hasClass('ez-selection-filter-item-selected'),
+                                "The item should have the selected class"
+                            );
+                            Y.Assert.isFalse(
+                                item.hasClass('ez-selection-filter-item-selected'),
+                                "The previously selected item should not have the selected class"
+                            );
+                        });
+                    });
+                    this.wait();
+                });
+            });
+            this.wait();
+        },
+    });
+
+    sourceChangeTest = new Y.Test.Case({
+        name: "eZ Selection Filter View test",
+
+        setUp: function () {
+            this.initialDOM = '<input type="text" id="input-selection-filter" class=""><ul class=""></ul>';
+            Y.one('.container').setHTML(this.initialDOM);
+            Y.config.doc.documentElement.focus();
+
+            this.view = new Y.eZ.SelectionFilterView({
+                container: '.container',
+                inputNode: '#input-selection-filter',
+                listNode: '.container ul',
+                source: source,
+                resultFilters: 'startsWith',
+                resultHighlighter: 'startsWith',
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+            delete this.view;
+        },
+
+        _assertDefaultList: function (source) {
+            var items = this.view.get('listNode').all(this.view.ITEM_TAG);
+
+            Y.Assert.areEqual(
+                source.length, items.size(),
+                "The list should contain as many items as in the source"
+            );
+
+            Y.Array.each(source, function (val) {
+                var item = this.view.get('listNode').one('[data-text="' + val + '"]');
+                Y.Assert.isObject(item, "The item for '" + val + "' is missing");
+                Y.Assert.areEqual(
+                    val, item.getContent()
+                );
+            }, this);
+        },
+
+        "Should render itself when the source array changes": function () {
+            var newSource = ['Foo Fighters', 'Led Zeppelin', 'AC/DC'];
+
+            this.view.render();
+            this.view.set('source', newSource);
+            this._assertDefaultList(newSource);
+        },
+    });
+
+
     Y.Test.Runner.setName("eZ Selection Filter View tests");
     Y.Test.Runner.add(viewTest);
     Y.Test.Runner.add(filterDisabledTest);
@@ -524,4 +844,6 @@ YUI.add('ez-selectionfilterview-tests', function (Y) {
     Y.Test.Runner.add(multipleSelectTest);
     Y.Test.Runner.add(multipleUnselectTest);
     Y.Test.Runner.add(sourceValidationTest);
+    Y.Test.Runner.add(objectSourceTest);
+    Y.Test.Runner.add(sourceChangeTest);
 }, '', {requires: ['test', 'node-event-simulate', 'ez-selectionfilterview']});
