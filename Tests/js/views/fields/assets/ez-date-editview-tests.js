8ce57eaef5415a2e869e3b6826d042b14bcef55b
Stephane DIOT <sdiot@live.fr>
2014-11-25 15:24:27 +0100

    ezp-21826 date field in the edit part of platformUI

---
+++ Tests/js/views/fields/assets/ez-date-editview-tests.js
@@ -0,0 +1,184 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: sd
+ * Date: 04/11/14
+ * Time: 13:49
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-date-editview-tests', function (Y) {
+    var viewTest, registerTest, getFieldTest;
+
+    viewTest = new Y.Test.Case({
+        name: "eZ date editView test",
+
+        _getFieldDefinition: function (required) {
+            return {
+                isRequired: required
+            };
+        },
+
+        setUp: function () {
+            this.field = {};
+            this.jsonContent = {};
+            this.jsonContentType = {};
+            this.jsonVersion = {};
+            this.content = new Y.Mock();
+            this.version = new Y.Mock();
+            this.contentType = new Y.Mock();
+            Y.Mock.expect(this.content, {
+                method: 'toJSON',
+                returns: this.jsonContent
+            });
+            Y.Mock.expect(this.version, {
+                method: 'toJSON',
+                returns: this.jsonVersion
+            });
+            Y.Mock.expect(this.contentType, {
+                method: 'toJSON',
+                returns: this.jsonContentType
+            });
+
+            this.view = new Y.eZ.DateEditView({
+                container: '.container',
+                field: this.field,
+                content: this.content,
+                version: this.version,
+                contentType: this.contentType
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+        },
+
+        _testAvailableVariables: function (required, expectRequired) {
+            var fieldDefinition = this._getFieldDefinition(required),
+                that = this;
+
+            this.view.set('fieldDefinition', fieldDefinition);
+
+            this.view.template = function (variables) {
+                Y.Assert.isObject(variables, "The template should receive some variables");
+                Y.Assert.areEqual(7, Y.Object.keys(variables).length, "The template should receive 6 variables");
+
+                Y.Assert.areSame(
+                    that.jsonContent, variables.content,
+                    "The content should be available in the field edit view template"
+                );
+                Y.Assert.areSame(
+                    that.jsonVersion, variables.version,
+                    "The version should be available in the field edit view template"
+                );
+                Y.Assert.areSame(
+                    that.jsonContentType, variables.contentType,
+                    "The contentType should be available in the field edit view template"
+                );
+                Y.Assert.areSame(
+                    fieldDefinition, variables.fieldDefinition,
+                    "The fieldDefinition should be available in the field edit view template"
+                );
+                Y.Assert.areSame(
+                    that.field, variables.field,
+                    "The field should be available in the field edit view template"
+                );
+
+                Y.Assert.areSame(expectRequired, variables.isRequired);
+                return '';
+            };
+            this.view.render();
+        },
+
+        "Test not required field": function () {
+            this._testAvailableVariables(false, false);
+        },
+
+        "Test required field": function () {
+            this._testAvailableVariables(true, true);
+        },
+
+        "Test validate no constraints": function () {
+            var fieldDefinition = this._getFieldDefinition(false),
+                input;
+
+            this.view.set('fieldDefinition', fieldDefinition);
+            this.view.render();
+
+            this.view.validate();
+            Y.Assert.isTrue(
+                this.view.isValid(),
+                "An empty input is valid"
+            );
+
+            input = Y.one('.container input');
+            input.set('value', '');
+            Y.Assert.isTrue(
+                this.view.isValid(),
+                "A empty input is valid"
+            );
+
+            input = Y.one('.container input');
+            input.set('value', '1986-09-08');
+
+            Y.Assert.isTrue(
+                this.view.isValid(),
+                "A non empty input is valid"
+            );
+        },
+
+        "Test validate required": function () {
+            var fieldDefinition = this._getFieldDefinition(true),
+                input;
+
+            this.view.set('fieldDefinition', fieldDefinition);
+            this.view.render();
+
+            input = Y.one('.container input');
+
+            input.set('value', '1986-09-08');
+            this.view.validate();
+            Y.Assert.isTrue(
+                this.view.isValid(),
+                "A non empty input is valid"
+            );
+
+            input.set('value', '');
+            this.view.validate();
+            Y.Assert.isFalse(
+                this.view.isValid(),
+                "An empty input is invalid"
+            );
+        }
+    });
+
+    Y.Test.Runner.setName("eZ Time Edit View tests");
+    Y.Test.Runner.add(viewTest);
+
+    getFieldTest = new Y.Test.Case(
+        Y.merge(Y.eZ.Test.GetFieldTests, {
+            fieldDefinition: {isRequired: false},
+            ViewConstructor: Y.eZ.DateEditView,
+            expectedValue: '1986-09-08',
+            convertedValue:526521600,
+
+            _setNewValue: function () {
+                this.view.get('container').one('input').set('value', this.expectedValue);
+            },
+
+            _assertCorrectFieldValue: function (fieldValue, msg) {
+                Y.Assert.isObject(fieldValue, 'the fieldValue should be an object');
+                Y.Assert.areSame(this.convertedValue, fieldValue.timestamp, 'the converted date should match the fieldValue timestamp')
+            },
+        })
+    );
+    Y.Test.Runner.add(getFieldTest);
+
+    registerTest = new Y.Test.Case(Y.eZ.EditViewRegisterTest);
+    registerTest.name = "Date Edit View registration test";
+    registerTest.viewType = Y.eZ.DateEditView;
+    registerTest.viewKey = "ezdate";
+    Y.Test.Runner.add(registerTest);
+}, '', {requires: ['test', 'getfield-tests', 'editviewregister-tests', 'ez-date-editview']});
