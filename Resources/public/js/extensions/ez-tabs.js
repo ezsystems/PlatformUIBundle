f7df87bdff58c756ce442d9453a0a3bcee2b4d8f
Damien Pobel <dp@ez.no>
2015-01-29 08:25:22 +0100

    EZP-23817: Changed the tab extension to automatically set the tap event handler

--- Resources/public/js/extensions/ez-tabs.js
+++ Resources/public/js/extensions/ez-tabs.js
@@ -14,13 +14,39 @@ YUI.add('ez-tabs', function (Y) {
     var TAB_IS_SELECTED = 'is-tab-selected',
         /**
          * Extension providing the `_selectTab` method which allows to handle a
-         * tab based interface.
+         * tab based interface. By default, it sets a tap event handler
+         * on '.ez-tabs .ez-tabs-label a' to switch from a tab to another.
          *
          * @namespace eZ
          * @class Tabs
          * @extensionfor Y.View
          */
-        Tabs = function () {};
+        Tabs = function () {},
+        _events = {
+            '.ez-tabs .ez-tabs-label a': {
+                'tap': '_uiTab'
+            }
+        };
+
+    Tabs.prototype.initializer = function () {
+        this.events = Y.merge(_events, this.events || {});
+    };
+
+    /**
+     * tap event handler on a tab label
+     *
+     * @method _uiTab
+     * @protected
+     * @param {Object} e tap event facade
+     */
+    Tabs.prototype._uiTab = function (e) {
+        e.preventDefault();
+        this._selectTab(
+            e.currentTarget.ancestor('.ez-tabs-label'),
+            e.currentTarget.getAttribute('href'),
+            this.get('container')
+        );
+    };
 
     /**
      * Selects the `tabLabel` and add the tab selected class on the `targetId`
