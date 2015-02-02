a72e64c52b61c66b44b1a56cd0c76ae03a5dc69d
Damien Pobel <dp@ez.no>
2015-02-02 16:04:49 +0100

    EZP-23098: Changed the navigation hub view service to inject the navigation item

--- Resources/public/js/views/services/ez-navigationhubviewservice.js
+++ Resources/public/js/views/services/ez-navigationhubviewservice.js
@@ -40,6 +40,32 @@ YUI.add('ez-navigationhubviewservice', function (Y) {
         },
 
         /**
+         * Returns the navigation items object.
+         *
+         * @method _getNavigationLocationItems
+         * @protected
+         * @param {String} title
+         * @param {String} identifier
+         * @param {String} locationId
+         * @return {Object}
+         */
+        _getNavigationLocationItems: function (title, identifier, locationId) {
+            return {
+                Constructor: Y.eZ.NavigationItemView,
+                config: {
+                    title: title,
+                    identifier: identifier,
+                    route: {
+                        name: 'viewLocation',
+                        params: {
+                            id: locationId,
+                        },
+                    },
+                },
+            };
+        },
+
+        /**
          * Returns the parameters for the navigation hub view
          *
          * @method getViewParameters
@@ -48,7 +74,110 @@ YUI.add('ez-navigationhubviewservice', function (Y) {
         _getViewParameters: function () {
             return {
                 user: this.get('app').get('user'),
+                createNavigationItems: this.get('createNavigationItems'),
+                deliverNavigationItems: this.get('deliverNavigationItems'),
+                optimizeNavigationItems: this.get('optimizeNavigationItems'),
             };
+        },
+
+        /**
+         * Adds a navigation item for the given zone
+         *
+         * @method addNavigationItem
+         * @param {Object} item a navigation item, see the description of the
+         * *NavigationItems attributes
+         * @param {String} zone the identifier of the zone
+         */
+        addNavigationItem: function (item, zone) {
+            this.get(zone + 'NavigationItems').push(item);
+        },
+
+        /**
+         * Removes a navigation item for the given zone
+         *
+         * @method removeNavigationItem
+         * @param {String} identifier the identifier of the navigation item to
+         * remove
+         * @param {String} zone the identifier of the zone
+         */
+        removeNavigationItem: function (identifier, zone) {
+            var attr = zone + 'NavigationItems';
+
+            this._set(attr, Y.Array.reject(this.get(attr), function (elt) {
+                return elt.config.identifier === identifier;
+            }));
+        },
+    }, {
+        ATTRS: {
+            /**
+             * Stores the navigation item objects for the 'create' zone. Each
+             * object must contain a `Constructor` property referencing
+             * the constructor function to use to build the navigation item
+             * view and a `config` property will be used as a configuration
+             * object for the navigation item view. This configuration must
+             * contain a `title` and an `identifier` properties.
+             *
+             * @attribute createNavigationItems
+             * @type Array
+             * @default array containing the object the 'Content structure' and
+             * 'Media library' items
+             * @readOnly
+             */
+            createNavigationItems: {
+                valueFn: function () {
+                    // TODO these location ids should be taken from the REST
+                    // root ressource instead of being hardcoded
+                    return [
+                        this._getNavigationLocationItems(
+                            "Content structure",
+                            "content-structure",
+                            "/api/ezp/v2/content/locations/1/2"
+                        ),
+                        this._getNavigationLocationItems(
+                            "Media library",
+                            "media-library",
+                            "/api/ezp/v2/content/locations/1/43"
+                        ),
+                    ];
+                },
+                readOnly: true,
+            },
+
+            /**
+             * Stores the navigation item objects for the 'optimize' zone. Each
+             * object must contain a `Constructor` property referencing
+             * the constructor function to use to build the navigation item
+             * view and a `config` property will be used as a configuration
+             * object for the navigation item view. This configuration must
+             * contain a `title` and an `identifier` properties.
+             *
+             * @attribute optimizeNavigationItems
+             * @type Array
+             * @default empty array
+             * @readOnly
+             */
+            optimizeNavigationItems: {
+                value: [],
+                readOnly: true,
+            },
+
+            /**
+             * Stores the navigation item objects for the 'deliver' zone. Each
+             * object must contain a `Constructor` property referencing
+             * the constructor function to use to build the navigation item
+             * view and a `config` property will be used as a configuration
+             * object for the navigation item view. This configuration must
+             * contain a `title` and an `identifier` properties.
+             *
+             * @attribute deliverNavigationItems
+             * @type Array
+             * @default empty array
+             * @readOnly
+             */
+            deliverNavigationItems: {
+                value: [],
+                readOnly: true,
+            },
         },
     });
 });
--- Tests/js/views/services/assets/ez-navigationhubviewservice-tests.js
+++ Tests/js/views/services/assets/ez-navigationhubviewservice-tests.js
@@ -3,7 +3,9 @@
  * For full copyright and license information view LICENSE file distributed with this source code.
  */
 YUI.add('ez-navigationhubviewservice-tests', function (Y) {
-    var getViewParametersTest, logOutEvtTest;
+    var getViewParametersTest, logOutEvtTest, defaultNavigationItemsTest,
+        addNavigationItemTest, removeNavigationItemTest,
+        Assert = Y.Assert;
 
     getViewParametersTest = new Y.Test.Case({
         name: "eZ Navigation Hub View Service getViewParameters test",
@@ -30,6 +32,29 @@ YUI.add('ez-navigationhubviewservice-tests', function (Y) {
                 "The view parameter should contain the app's user"
             );
             Y.Mock.verify(this.app);
+        },
+
+        _testNavigationItems: function (zone) {
+            var items = [];
+
+            this.service._set(zone + 'NavigationItems', items);
+
+            Assert.areSame(
+                items, this.service.getViewParameters()[zone + 'NavigationItems'],
+                "The navigation items for the zone '" + zone + "' should be available in the view parameters"
+            );
+        },
+
+        "Should return an object containing the 'create' navigation items": function () {
+            this._testNavigationItems('create');
+        },
+
+        "Should return an object containing the 'optimize' navigation items": function () {
+            this._testNavigationItems('optimize');
+        },
+
+        "Should return an object containing the 'deliver' navigation items": function () {
+            this._testNavigationItems('deliver');
         },
     });
 
@@ -64,8 +89,179 @@ YUI.add('ez-navigationhubviewservice-tests', function (Y) {
         },
     });
 
+    defaultNavigationItemsTest = new Y.Test.Case({
+        name: "eZ Navigation Hub View Service default navigation items",
+
+        setUp: function () {
+            this.service = new Y.eZ.NavigationHubViewService();
+        },
+
+        tearDown: function () {
+            this.service.destroy();
+            delete this.service;
+        },
+
+        _assertLocationNavigationItem: function (item, title, identifier, locationId) {
+            Assert.areSame(
+                Y.eZ.NavigationItemView, item.Constructor,
+                "The constructor should be eZ.NavigationItemView"
+            );
+            Assert.areEqual(
+                title, item.config.title,
+                "The navigation item title does not match"
+            );
+            Assert.areEqual(
+                identifier, item.config.identifier,
+                "The navigation item identifier does not match"
+            );
+            Assert.areEqual(
+                "viewLocation", item.config.route.name,
+                "The navigation item route name does not match"
+            );
+            Assert.areEqual(
+                locationId, item.config.route.params.id,
+                "The navigation item location id does not match"
+            );
+        },
+
+        "'create' zone": function () {
+            var value = this.service.get('createNavigationItems');
+
+            Assert.isArray(value, "The createNavigationItems should contain an array");
+            Assert.areEqual(
+                2, value.length,
+                "2 items should be configured by default for the create zone"
+            );
+            this._assertLocationNavigationItem(
+                value[0], "Content structure", "content-structure", "/api/ezp/v2/content/locations/1/2"
+            );
+            this._assertLocationNavigationItem(
+                value[1], "Media library", "media-library", "/api/ezp/v2/content/locations/1/43"
+            );
+        },
+
+        "'optimize' zone": function () {
+            var value = this.service.get('optimizeNavigationItems');
+
+            Assert.isArray(value, "The optimizeNavigationItems should contain an array");
+            Assert.areEqual(
+                0, value.length,
+                "optimize zone should be empty"
+            );
+        },
+
+        "'deliver' zone": function () {
+            var value = this.service.get('deliverNavigationItems');
+
+            Assert.isArray(value, "The deliverNavigationItems should contain an array");
+            Assert.areEqual(
+                0, value.length,
+                "deliver zone should be empty"
+            );
+        },
+    });
+
+    addNavigationItemTest = new Y.Test.Case({
+        name: "eZ Navigation Hub View Service add navigation item tests",
+
+        setUp: function () {
+            this.service = new Y.eZ.NavigationHubViewService();
+        },
+
+        tearDown: function () {
+            this.service.destroy();
+            delete this.service;
+        },
+
+        _testAttribute: function (zone) {
+            var attr = zone + 'NavigationItems',
+                item = {},
+                value = this.service.get(attr),
+                origLength = value.length;
+
+            this.service.addNavigationItem(item, zone);
+
+            Assert.areEqual(
+                origLength + 1, this.service.get(attr).length,
+                "The item should be added to the zone '" + zone + "'"
+            );
+            Assert.areSame(
+                this.service.get(attr)[origLength], item,
+                "The item should be added to the zone '" + zone + "'"
+            );
+        },
+
+        "Should add the navigation item to the 'create' zone": function () {
+            this._testAttribute('create');
+        },
+
+        "Should add the navigation item to the 'optimize' zone": function () {
+            this._testAttribute('optimize');
+        },
+
+        "Should add the navigation item to the 'deliver' zone": function () {
+            this._testAttribute('deliver');
+        },
+    });
+
+    removeNavigationItemTest = new Y.Test.Case({
+        name: "eZ Navigation Hub View Service remove navigation item test",
+
+        setUp: function () {
+            this.service = new Y.eZ.NavigationHubViewService();
+            this.createIdentifier = 'peppa-pig';
+            this.optimizeIdentifier = 'ben-et-holly';
+            this.deliverIdentifier = 'paw-patrol';
+            this.service.addNavigationItem(
+                {config: {identifier: this.createIdentifier}},
+                'create'
+            );
+            this.service.addNavigationItem(
+                {config: {identifier: this.deliverIdentifier}},
+                'deliver'
+            );
+            this.service.addNavigationItem(
+                {config: {identifier: this.optimizeIdentifier}},
+                'optimize'
+            );
+        },
+
+        tearDown: function () {
+            this.service.destroy();
+            delete this.service;
+        },
+
+        _testAttribute: function (zone) {
+            var identifier = this[zone + "Identifier"];
+
+            this.service.removeNavigationItem(identifier, zone);
+
+            Y.Array.each(this.service.get(zone + "NavigationItems"), function (item) {
+                Assert.areNotEqual(
+                    identifier, item.config.identifier,
+                    identifier + " should have been removed"
+                );
+            });
+        },
+
+        "Should remove the navigation item to the 'create' zone": function () {
+            this._testAttribute('create');
+        },
+
+        "Should remove the navigation item to the 'optimize' zone": function () {
+            this._testAttribute('optimize');
+        },
+
+        "Should remove the navigation item to the 'deliver' zone": function () {
+            this._testAttribute('deliver');
+        },
+    });
+
 
     Y.Test.Runner.setName("eZ Navigation Hub View Service tests");
     Y.Test.Runner.add(getViewParametersTest);
     Y.Test.Runner.add(logOutEvtTest);
+    Y.Test.Runner.add(defaultNavigationItemsTest);
+    Y.Test.Runner.add(addNavigationItemTest);
+    Y.Test.Runner.add(removeNavigationItemTest);
 }, '', {requires: ['test', 'ez-navigationhubviewservice']});
