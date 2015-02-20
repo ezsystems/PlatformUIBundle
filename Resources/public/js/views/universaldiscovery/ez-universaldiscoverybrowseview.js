caf0f80b99f01dbb233802f6ceed87870dea53ce
Damien Pobel <dp@ez.no>
2015-02-20 14:00:00 +0100

    EZP-23817: Implemented the universal discovery browse method

--- Resources/public/js/views/universaldiscovery/ez-universaldiscoverybrowseview.js
+++ Resources/public/js/views/universaldiscovery/ez-universaldiscoverybrowseview.js
@@ -12,9 +12,8 @@ YUI.add('ez-universaldiscoverybrowseview', function (Y) {
     Y.namespace('eZ');
 
     /**
-     * The universal discovery browse method view. It allows the user to pick
-     * one or several contents in the repository by browsing using a tree and
-     * the children list.
+     * The universal discovery browse method view. It allows the user to pick a
+     * content in the repository by browsing using a tree.
      *
      * @namespace eZ
      * @class UniversalDiscoveryBrowseView
@@ -23,16 +22,116 @@ YUI.add('ez-universaldiscoverybrowseview', function (Y) {
      */
     Y.eZ.UniversalDiscoveryBrowseView = Y.Base.create('universalDiscoveryBrowseView', Y.eZ.UniversalDiscoveryMethodBaseView, [], {
         initializer: function () {
-            this._set('title', 'Browse');
-            this._set('identifier', 'browse');
+            this.on('*:treeNavigate', this._selectContent);
+        },
+
+        /**
+         * Custom reset implementation to explicitely reset the sub views.
+         *
+         * @method reset
+         * @param {String} [name]
+         */
+        reset: function (name) {
+            if ( name === 'treeView' ) {
+                this.get('treeView').reset();
+                return;
+            }
+            if ( name === 'selectedView' ) {
+                this.get('selectedView').reset();
+                return;
+            }
+            this.constructor.superclass.reset.apply(this, arguments);
         },
 
         render: function () {
-            this.get('container').setHTML(this.template({
-                title: this.get('title'),
-                identifier: this.get('identifier'),
-            }));
+            var container = this.get('container');
+
+            container.setHTML(this.template());
+            container.one('.ez-ud-browse-tree').append(
+                this.get('treeView').render().get('container')
+            );
+            container.one('.ez-ud-browse-selected').append(
+                this.get('selectedView').render().get('container')
+            );
             return this;
+        },
+
+        /**
+         * `treeNavigate` event handler. It fires the `selectedContent` event
+         * and set the content structure on the selected view so that it is
+         * displayed.
+         *
+         * @method _selectContent
+         * @protected
+         * @param {EventFacade} e
+         */
+        _selectContent: function (e) {
+            var node = e.tree.getNodeById(e.nodeId);
+
+            e.preventDefault();
+            /**
+             * Fired when a content is selected. The event facade provides the
+             * content structure (the content, location and content type models)
+             *
+             * @event selectContent
+             * @param selection {Object}
+             * @param selection.content {eZ.Content}
+             * @param selection.location {eZ.Location}
+             * @param selection.contentType {eZ.ContentType}
+             */
+            this.fire('selectContent', {
+                selection: node.data,
+            });
+            this.get('selectedView').set('contentStruct', node.data);
+        },
+    }, {
+        ATTRS: {
+            /**
+             * @attribute title
+             * @default 'Browse'
+             */
+            title: {
+                value: 'Browse',
+                readOnly: true,
+            },
+
+            /**
+             * @attribute identifier
+             * @default 'browse'
+             */
+            identifier: {
+                value: 'browse',
+                readOnly: true,
+            },
+
+            /**
+             * Holds the tree view
+             *
+             * @attribute treeView
+             * @type {eZ.TreeView}
+             */
+            treeView: {
+                valueFn: function () {
+                    return new Y.eZ.TreeView({
+                        bubbleTargets: this,
+                    });
+                },
+            },
+
+            /**
+             * Holds the selected view that displays the currently selected
+             * content (if any)
+             *
+             * @attribute selectedView
+             * @type {eZ.UniversalDiscoverySelectedView}
+             */
+            selectedView: {
+                valueFn: function () {
+                    return new Y.eZ.UniversalDiscoverySelectedView({
+                        bubbleTargets: this,
+                    });
+                },
+            },
         },
     });
 });
---
+++ Tests/js/views/universaldiscovery/assets/ez-universaldiscoverybrowseview-tests.js
@@ -0,0 +1,259 @@
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-universaldiscoverybrowseview-tests', function (Y) {
+    var resetTest, defaultSubViewTest, treeNavigateTest, renderTest,
+        Assert = Y.Assert, Mock = Y.Mock;
+
+    resetTest = new Y.Test.Case({
+        name: 'eZ Universal Discovery Browse reset tests',
+
+        setUp: function () {
+            this.treeView = new Mock();
+            this.selectedView = new Mock();
+            Mock.expect(this.selectedView, {
+                method: 'reset',
+            });
+            Mock.expect(this.treeView, {
+                method: 'reset',
+            });
+            this.view = new Y.eZ.UniversalDiscoveryBrowseView({
+                treeView: this.treeView,
+                selectedView: this.selectedView,
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+            delete this.view;
+            delete this.treeView;
+            delete this.selectedView;
+        },
+
+        "Should keep and reset the treeView and the selectedView": function () {
+            this.view.reset();
+            Mock.verify(this.treeView);
+            Mock.verify(this.selectedView);
+            Assert.areSame(
+                this.treeView, this.view.get('treeView'),
+                "The treeView should be kept"
+            );
+            Assert.areSame(
+                this.selectedView, this.view.get('selectedView'),
+                "The selectedView should be kept"
+            );
+        },
+
+        "Should keep the title and identifier": function () {
+            var title = this.view.get('title'),
+                identifier = this.view.get('identifier');
+
+            this.view.reset();
+            Assert.areEqual(
+                title, this.view.get('title'),
+                "The title should be kept intact"
+            );
+            Assert.areEqual(
+                identifier, this.view.get('identifier'),
+                "The identifier should be kept intact"
+            );
+            Mock.verify(this.treeView);
+            Mock.verify(this.selectedView);
+        },
+    });
+
+    defaultSubViewTest = new Y.Test.Case({
+        name: 'eZ Universal Discovery Browse default sub views tests',
+                       
+        setUp: function () {
+            Y.eZ.TreeView = Y.Base.create('treeView', Y.View, [], {});
+            Y.eZ.UniversalDiscoverySelectedView = Y.Base.create('selectedView', Y.View, [], {});
+            this.view = new Y.eZ.UniversalDiscoveryBrowseView();
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+            delete this.view;
+            delete Y.eZ.TreeView;
+            delete Y.eZ.UniversalDiscoverySelectedView;
+        },
+
+        "treeView should be an instance of eZ.TreeView": function () {
+            Assert.isInstanceOf(
+                Y.eZ.TreeView, this.view.get('treeView'),
+                "The treeView attribute value should an instance of eZ.TreeView"
+            );
+        },
+
+        "selectedView should be an instance of eZ.UniversalDiscoverySelectedView": function () {
+            Assert.isInstanceOf(
+                Y.eZ.UniversalDiscoverySelectedView, this.view.get('selectedView'),
+                "The selectedView attribute value should an instance of eZ.UniversalDiscoverySelectedView"
+            );
+        },
+
+        "Should be a bubble target of the treeView": function () {
+            var bubble = false;
+
+            this.view.on('*:whatever', function () {
+                bubble = true;
+            });
+            this.view.get('treeView').fire('whatever');
+            Assert.isTrue(bubble, "The event should bubble to the browse view");
+        },
+
+        "Should be a bubble target of the selectedView": function () {
+            var bubble = false;
+
+            this.view.on('*:whatever', function () {
+                bubble = true;
+            });
+            this.view.get('selectedView').fire('whatever');
+            Assert.isTrue(bubble, "The event should bubble to the browse view");
+        },
+    });
+
+    treeNavigateTest = new Y.Test.Case({
+        name: 'eZ Universal Discovery Browse treeNavigate event tests',
+
+        setUp: function () {
+            this.selectedView = new Mock();
+            this.treeView = new Mock();
+            this.view = new Y.eZ.UniversalDiscoveryBrowseView({
+                selectedView: this.selectedView,
+                treeView: this.treeView,
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+            delete this.view;
+            delete this.selectedView;
+            delete this.treeView;
+        },
+
+        "Should fire the `selectContent` event and update the selectedView": function () {
+            var tree = new Mock(),
+                nodeData = {},
+                nodeId = 42,
+                selectContent = false;
+
+            Mock.expect(tree, {
+                method: 'getNodeById',
+                args: [nodeId],
+                returns: {data: nodeData},
+            });
+            Mock.expect(this.selectedView, {
+                method: 'set',
+                args: ['contentStruct', nodeData],
+            });
+
+            this.view.after('*:treeNavigate', function (e) {
+                Assert.isTrue(
+                    !!e.prevented,
+                    "The treeNavigate event should have been prevented"
+                );
+            });
+
+            this.view.on('selectContent', function (e) {
+                selectContent = true;
+
+                Assert.areSame(
+                    nodeData, e.selection,
+                    "The node data should be provided in the selectContent event facade"
+                );
+            });
+
+            this.view.fire('whatever:treeNavigate', {
+                nodeId: nodeId,
+                tree: tree,
+            });
+
+            Assert.isTrue(selectContent, "The selectContent event should have been fired");
+            Mock.verify(tree);
+            Mock.verify(this.selectedView);
+        },
+    });
+
+    renderTest = new Y.Test.Case({
+        name: 'eZ Universal Discovery Browse render tests',
+                       
+        setUp: function () {
+            var that = this;
+
+            this.treeViewRendered = false;
+            this.selectedViewRendered = false;
+            Y.eZ.TreeView = Y.Base.create('treeView', Y.View, [], {
+                render: function () {
+                    that.treeViewRendered = true;
+                    return this;
+                },
+            });
+            Y.eZ.UniversalDiscoverySelectedView = Y.Base.create('selectedView', Y.View, [], {
+                render: function () {
+                    that.selectedViewRendered = true;
+                    return this;
+                },
+            });
+            this.view = new Y.eZ.UniversalDiscoveryBrowseView();
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+            delete this.view;
+            delete Y.eZ.TreeView;
+            delete Y.eZ.UniversalDiscoverySelectedView;
+        },
+
+        "Should use the template": function () {
+            var origTpl = this.view.template,
+                templateCalled = false;
+
+            this.view.template = function () {
+                templateCalled = true;
+                return origTpl.apply(this, arguments);
+            };
+            this.view.render();
+
+            Assert.isTrue(
+                templateCalled, "The template should have been used to render the view"
+            );
+        },
+
+        "Should render the treeView and the selectedView": function () {
+            var container = this.view.get('container'),
+                treeViewContainer = this.view.get('treeView').get('container'),
+                selectedViewContainer = this.view.get('selectedView').get('container');
+
+            this.view.render();
+
+            Assert.isTrue(this.treeViewRendered, "The treeView should have been rendered");
+            Assert.isTrue(this.selectedViewRendered, "The selectedView should have been rendered");
+
+            Assert.isTrue(
+                container.contains(treeViewContainer),
+                "The rendered treeView should be added to the browse view"
+            );
+            Assert.isTrue(
+                container.contains(selectedViewContainer),
+                "The rendered selectedView should be added to the browse view"
+            );
+
+            Assert.isTrue(
+                treeViewContainer.get('parentNode').hasClass('ez-ud-browse-tree'),
+                "The treeView should be added in the ez-ud-browse-tree element"
+            );
+            Assert.isTrue(
+                selectedViewContainer.get('parentNode').hasClass('ez-ud-browse-selected'),
+                "The selectedView should be added in the ez-ud-browse-selected element"
+            );
+        },
+    });
+
+    Y.Test.Runner.setName("eZ Universal Discovery Browse View tests");
+    Y.Test.Runner.add(resetTest);
+    Y.Test.Runner.add(defaultSubViewTest);
+    Y.Test.Runner.add(treeNavigateTest);
+    Y.Test.Runner.add(renderTest);
+}, '', {requires: ['test', 'view', 'ez-universaldiscoverybrowseview']});
