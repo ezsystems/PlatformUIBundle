2531a0b29e6938f5d4bed5dd267b19af662f82f1
Damien Pobel <dp@ez.no>
2015-05-28 11:32:49 +0200

    Fix EZP-24418: ServerSideView multiple submit buttons support

--- Tests/js/views/assets/ez-serversideview-tests.js
+++ Tests/js/views/assets/ez-serversideview-tests.js
@@ -3,7 +3,7 @@
  * For full copyright and license information view LICENSE file distributed with this source code.
  */
 YUI.add('ez-serversideview-tests', function (Y) {
-    var viewTest, tabTest, formTest,
+    var viewTest, tabTest, formTest, formSerializeTest,
         Assert = Y.Assert;
 
     viewTest = new Y.Test.Case({
@@ -141,6 +141,10 @@ YUI.add('ez-serversideview-tests', function (Y) {
                     form, e.form,
                     "The form should be provided"
                 );
+                Assert.isObject(
+                    e.formData,
+                    "The form should be serialzed"
+                );
                 e.originalEvent.preventDefault();
             });
             this.view.render();
@@ -155,9 +159,181 @@ YUI.add('ez-serversideview-tests', function (Y) {
         },
     });
 
+    formSerializeTest = new Y.Test.Case({
+        name: "eZ Server Side view serialize form tests",
+
+        setUp: function () {
+            this.view = new Y.eZ.ServerSideView({
+                container: Y.one('.form-test-container'),
+            });
+        },
+
+        "Should serialize the text inputs": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="text" value="1" name="one">';
+            html += '<input type="text" value="2" name="two">';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"one": "1", "two": "2"}, 'form input[type="submit"]');
+        },
+
+        "Should serialize the textareas": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<textarea name="one">1</textarea>';
+            html += '<textarea name="two">two</textarea>';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"one": "1", "two": "two"}, 'form input[type="submit"]');
+        },
+
+        "Should serialize the select": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<select name="one"><option value="1" selected>really 1</option>';
+            html += '<option value="2">2</option></select>';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"one": "1"}, 'form input[type="submit"]');
+        },
+
+        "Should serialize the checkbox input": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="checkbox" value="1" name="one" checked>';
+            html += '<input type="checkbox" value="2" name="two">';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"one": "1"}, 'form input[type="submit"]');
+        },
+
+        "Should serialize the radio input": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="radio" value="1" name="one" checked>';
+            html += '<input type="radio" value="2" name="two">';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"one": "1"}, 'form input[type="submit"]');
+        },
+
+        "Should ignore buttons": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="button" value="1" name="one" checked>';
+            html += '<input type="reset" value="2" name="two">';
+            html += '<input type="image" value="2" name="two">';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {}, 'form input[type="submit"]');
+        },
+
+        "Should serialize the used input button": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="submit" value="Submit" name="submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"submit": ""}, 'form input[type="submit"]', this._forceActiveElement);
+        },
+
+        "Should serialize the used 'image' input button": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="image" value="Submit" name="submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {"submit": ""}, 'form input[type="image"]', this._forceActiveElement);
+        },
+
+        "Should serialize the used button": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<button type="submit" name="submit">Submit!</button>';
+            html += '</form>';
+
+            this._serializeTest(html, {"submit": ""}, 'form button[type="submit"]', this._forceActiveElement);
+        },
+
+        "Should ignore the used input button without a name": function () {
+            var html;
+
+            html = '<form method="post" action="">';
+            html += '<input type="submit" value="Submit">';
+            html += '</form>';
+
+            this._serializeTest(html, {}, 'form input[type="submit"]', this._forceActiveElement);
+        },
+
+        _forceActiveElement: function (submit) {
+            submit.focus();
+        },
+
+        _serializeTest: function (html, expectedData, submitSelector, preTestFunc) {
+            var c = this.view.get('container'),
+                that = this,
+                submit, submitFormEvent = false;
+
+            this.view.set('html', html);
+            this.view.on('submitForm', function (e) {
+                e.originalEvent.preventDefault();
+                submitFormEvent = true;
+
+                Assert.isObject(
+                    e.formData,
+                    "The form should be serialzed"
+                );
+                that._assertFormData(e.formData, expectedData);
+            });
+            this.view.render();
+            submit = c.one(submitSelector);
+            if ( preTestFunc ) {
+                preTestFunc.call(this, submit);
+            }
+            submit.simulate('click');
+            Assert.isTrue(submitFormEvent, "The submitForm should have been fired");
+        },
+
+        _assertFormData: function (formData, expected) {
+            var expectedLength = Y.Object.keys(expected).length;
+
+            Assert.areEqual(
+                Y.Object.keys(formData).length,
+                expectedLength,
+                "The formData should have " + expectedLength + " entries"
+            );
+            Y.Object.each(formData, function (data, key) {
+                Assert.areSame(
+                    data, expected[key],
+                    "The " + key + " entry should be '" + expected[key] + "'"
+                );
+            });
+        },
+
+        tearDown: function () {
+            this.view.destroy();
+        },
+    });
 
     Y.Test.Runner.setName("eZ Server Side View tests");
     Y.Test.Runner.add(viewTest);
     Y.Test.Runner.add(tabTest);
     Y.Test.Runner.add(formTest);
+    Y.Test.Runner.add(formSerializeTest);
 }, '', {requires: ['test', 'node-event-simulate', 'ez-serversideview']});
