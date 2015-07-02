d86afb5a8f538432b488940ae310fd487c76cd85
Jérôme Vieilledent <lolautruche@gmail.com>
2015-07-02 11:51:07 +0200

    Fixed wrong redirection when discarding a newly created ContentTypeDraft

--- Form/Processor/ContentTypeFormProcessor.php
+++ Form/Processor/ContentTypeFormProcessor.php
@@ -9,6 +9,8 @@
 
 namespace EzSystems\PlatformUIBundle\Form\Processor;
 
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use eZ\Publish\API\Repository\Values\ContentType\ContentTypeDraft;
 use EzSystems\PlatformUIBundle\Notification\NotificationPoolInterface;
 use EzSystems\PlatformUIBundle\Notification\TranslatableNotificationMessage;
@@ -30,10 +32,16 @@ class ContentTypeFormProcessor implements EventSubscriberInterface
      */
     private $notificationPool;
 
-    public function __construct(RouterInterface $router, NotificationPoolInterface $notificationPool)
+    /**
+     * @var ContentTypeService
+     */
+    private $contentTypeService;
+
+    public function __construct(RouterInterface $router, NotificationPoolInterface $notificationPool, ContentTypeService $contentTypeService)
     {
         $this->router = $router;
         $this->notificationPool = $notificationPool;
+        $this->contentTypeService = $contentTypeService;
     }
 
     public static function getSubscribedEvents()
@@ -64,13 +72,25 @@ class ContentTypeFormProcessor implements EventSubscriberInterface
 
     public function processRemoveContentTypeDraft(FormActionEvent $event)
     {
-        $event->setResponse(
-            $this->generateRedirectResponse(
-                $event->getData()->contentTypeDraft,
-                $event->getOption('languageCode')
-            )
-        );
+        /** @var ContentTypeDraft $contentTypeDraft */
+        $contentTypeDraft = $event->getData()->contentTypeDraft;
+        $languageCode = $event->getOption('languageCode');
+        // Redirect response will be different if we're dealing with an existing ContentType or a newly created one, which has been discarded.
+        try {
+            // This will throw a NotFoundException if a published version doesn't exist for this ContentType.
+            $this->contentTypeService->loadContentType($contentTypeDraft->id);
+            $response = $this->generateRedirectResponse($contentTypeDraft, $languageCode);
+        } catch (NotFoundException $e) {
+            // ContentTypeDraft was newly created, but then discarded.
+            // Redirect to the ContentTypeGroup view.
+            $response = new RedirectResponse(
+                $this->router->generate('admin_contenttypeGroupView', [
+                    'contentTypeGroupId' => $contentTypeDraft->contentTypeGroups[0]->id,
+                ])
+            );
+        }
 
+        $event->setResponse($response);
         $this->addNotification('content_type.notification.draft_removed');
     }
 
