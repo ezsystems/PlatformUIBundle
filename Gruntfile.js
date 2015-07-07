30e799307a76db72d5e0dd6007161482c658354c
Yannick ROGER <yannick.roger@ez.no>
2015-07-07 16:31:35 +0200

    Updated livedoc logo

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function(grunt) {
                 description: '<%= pkg.description %>',
                 version: '<%= pkg.version %>',
                 url: '<%= pkg.repository %>',
-                logo: "http://ez.no/extension/ez_ezno/design/ezno_2011/images/ez-logo.png",
+                logo: "http://ez.no/extension/ez_ezno/design/ezno_2014/images/ez-logo.png",
                 options: {
                     paths: [
                         "./Resources/public/js/apps",
