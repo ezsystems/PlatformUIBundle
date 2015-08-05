2216e1a5a37ddd37e3c68e3e6d71e3994b72396e
André R <ar@ez.no>
2015-08-05 17:46:46 +0200

    [Travis] Added Behat testing for PlatformUI

--- bin/travis/setupphp.sh
+++ bin/travis/setupphp.sh
@@ -2,4 +2,4 @@
 
 add-apt-repository -y ppa:ondrej/php5
 apt-get update -q
-apt-get install -y php5-cli php5-common php5-gd php5-xsl php5-intl
+apt-get install -q -y --force-yes php5-cli php5-common php5-gd php5-xsl php5-intl
