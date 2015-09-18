d0e739ee0535e68c6218ffdf4393ee6bdddb8bfc
Damien Pobel <dp@ez.no>
2015-09-18 18:55:22 +0200

    EZP-24824: Added a location search plugin

---
+++ Resources/public/js/views/services/plugins/ez-locationsearchplugin.js
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-locationsearchplugin', function (Y) {
+    "use strict";
+    /**
+     * Provides the location search plugin
+     *
+     * @module ez-locationsearchplugin
+     */
+    Y.namespace('eZ.Plugin');
+
+    /**
+     * Location search plugin. It listens to the `locationSearch` event and
+     * execute the corresponding location search.
+     *
+     * @namespace eZ.Plugin
+     * @class LocationSearch
+     * @constructor
+     * @extends eZ.Plugin.ViewServiceBase
+     */
+    Y.eZ.Plugin.LocationSearch = Y.Base.create('locationSearchPlugin', Y.eZ.Plugin.ViewServiceBase, [], {
+        initializer: function () {
+            this.onHostEvent('*:locationSearch', this._doLocationSearch);
+        },
+
+        /**
+         * `locationSearch` event handler. It executes the location search and
+         * set the result on the target of the event.
+         *
+         * @method _doLocationSearch
+         * @protected
+         * @param {EventFacade} e
+         */
+        _doLocationSearch: function (e) {
+            var service = this.get('host'),
+                listView = e.target,
+                contentService = service.get('capi').getContentService(),
+                query;
+
+            query = contentService.newViewCreateStruct(e.viewName, 'LocationQuery');
+            // TODO ViewCreateStruct should expose an API
+            // see https://jira.ez.no/browse/EZP-24808
+            query.body.ViewInput.LocationQuery.Criteria = e.search.criteria;
+
+            // not yet supported by the REST API
+            // see eZ.SubItemListView and https://jira.ez.no/browse/EZP-24315
+            // query.body.ViewInput.LocationQuery.SortClauses = e.search.sortClauses;
+            
+            contentService.createView(query, Y.bind(function (error, result) {
+                var attrs = {'loadingError': true};
+
+                if ( !error ) {
+                    attrs.loadingError = false;
+                    attrs[e.resultAttribute] = this._parseSearchResult(result);
+                }
+
+                listView.setAttrs(attrs);
+            }, this));
+        },
+
+        /**
+         * Parses the search result to create the Location model.
+         *
+         * @method _parseSearchResult
+         * @protected
+         * @param {Response} result the CAPI Response
+         */
+        _parseSearchResult: function (result) {
+            var ret = [];
+
+            Y.Array.each(result.document.View.Result.searchHits.searchHit, function (hit) {
+                ret.push(this._createLocation(hit));
+            }, this);
+
+            return ret;
+        },
+
+        /**
+         * Creates a Location for the searchHit
+         *
+         * @method _createLocation
+         * @param {Object} searchHit
+         * @protected
+         * @return {eZ.Location}
+         */
+        _createLocation: function (searchHit) {
+            var Location = this.get('locationModelConstructor'),
+                location;
+
+            location = new Location();
+            location.loadFromHash(searchHit.value.Location);
+
+            return location;
+        },
+    }, {
+        NS: 'locationSearch',
+
+        ATTRS: {
+            /**
+             * Holds the eZ.Location constructor function
+             *
+             * @attribute locationModelConstructor
+             * @type {Function}
+             * @default Y.eZ.Location
+             */
+            locationModelConstructor: {
+                value: Y.eZ.Location
+            }
+        },
+    });
+
+    Y.eZ.PluginRegistry.registerPlugin(
+        Y.eZ.Plugin.LocationSearch, ['locationViewViewService']
+    );
+});
---
+++ Tests/js/views/services/plugins/assets/ez-locationsearchplugin-tests.js
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-locationsearchplugin-tests', function (Y) {
+    var tests, registerTest,
+        Assert = Y.Assert, Mock = Y.Mock;
+
+    tests = new Y.Test.Case({
+        name: "eZ Location Search Plugin event tests",
+
+        setUp: function () {
+            this.LocationModelConstructor = function () {};
+            this.capi = new Mock();
+            this.contentService = new Mock();
+            this.viewName = 'REST-View-Name';
+            this.query = {body: {ViewInput: {LocationQuery: {}}}};
+
+            Mock.expect(this.capi, {
+                method: 'getContentService',
+                returns: this.contentService,
+            });
+            Mock.expect(this.contentService, {
+                method: 'newViewCreateStruct',
+                args: [this.viewName, 'LocationQuery'],
+                returns: this.query,
+            });
+            this.service = new Y.Base();
+            this.service.set('capi', this.capi);
+            this.view = new Y.Base({bubbleTargets: this.service});
+            this.plugin = new Y.eZ.Plugin.LocationSearch({
+                host: this.service,
+                locationModelConstructor: this.LocationModelConstructor,
+            });
+        },
+
+        tearDown: function () {
+            this.service.destroy();
+            this.plugin.destroy();
+            delete this.service;
+            delete this.plugin;
+            delete this.capi;
+            delete this.contentService;
+            delete this.query;
+        },
+
+        "Should create a LocationQuery with the given name and criteria": function () {
+            var criteria = {};
+
+            Mock.expect(this.contentService, {
+                method: 'createView',
+                args: [this.query, Mock.Value.Function],
+                run: function (query, cb) {
+                    Assert.areSame(
+                        criteria,
+                        query.body.ViewInput.LocationQuery.Criteria,
+                        "The criteria should be set on the view create struct"
+                    );
+                }
+            });
+
+            this.view.fire('locationSearch', {
+                viewName: this.viewName,
+                search: {
+                    criteria: criteria
+                }
+            });
+        },
+
+        "Should handle the search error": function () {
+            Mock.expect(this.contentService, {
+                method: 'createView',
+                args: [this.query, Mock.Value.Function],
+                run: function (query, cb) {
+                    cb(true);
+                }
+            });
+
+            this.view.fire('locationSearch', {
+                viewName: this.viewName,
+                search: {
+                    criteria: {},
+                }
+            });
+
+            Assert.isTrue(
+                this.view.get('loadingError'),
+                "The view should get the loadingError flag set to true"
+            );
+        },
+
+        "Should set the result on the target": function () {
+            var response = {
+                    document: {
+                        View: {
+                            Result: {
+                                searchHits: {
+                                    searchHit: [{
+                                        value: {
+                                            Location: {}
+                                        },
+                                    }, {
+                                        value: {
+                                            Location: {}
+                                        }
+                                    }]
+                                }
+                            }
+                        }
+                    }
+                },
+                resultAttr = 'whateverAttr';
+
+            this.LocationModelConstructor.prototype.loadFromHash = function (hash) {
+                this.hash = hash;
+            };
+
+            this.service.set('locationModelConstructor', this.LocationModelConstructor);
+            Mock.expect(this.contentService, {
+                method: 'createView',
+                args: [this.query, Mock.Value.Function],
+                run: function (query, cb) {
+                    cb(false, response);
+                }
+            });
+            
+            this.view.fire('locationSearch', {
+                viewName: this.viewName,
+                resultAttribute: resultAttr,
+                search: {
+                    criteria: {},
+                }
+            });
+
+            Assert.isFalse(
+                this.view.get('loadingError'),
+                "The loadingError flag should be false"
+            );
+            Assert.isArray(
+                this.view.get(resultAttr),
+                "The result should set in the event resultAttribute"
+            );
+
+            Y.Array.each(this.view.get(resultAttr), function (value, i) {
+                Assert.isInstanceOf(
+                    this.LocationModelConstructor,
+                    value,
+                    "The result value should be an instance of the model"
+                );
+                Assert.areSame(
+                    response.document.View.Result.searchHits.searchHit[i].value.Location,
+                    value.hash,
+                    "The location should be created from the hash"
+                );
+            }, this);
+        },
+    });
+
+    registerTest = new Y.Test.Case(Y.eZ.Test.PluginRegisterTest);
+    registerTest.Plugin = Y.eZ.Plugin.LocationSearch;
+    registerTest.components = ['locationViewViewService'];
+
+    Y.Test.Runner.setName("eZ Location Search Plugin tests");
+    Y.Test.Runner.add(tests);
+    Y.Test.Runner.add(registerTest);
+}, '', {requires: ['test', 'base', 'ez-locationsearchplugin', 'ez-pluginregister-tests']});
