0e1178a0080416c3b80f220564fed9ecb84ad88e
Damien Pobel <dp@ez.no>
2015-10-02 18:41:29 +0200

    EZP-24894: Trigger the universal discovery widget when adding an embed

---
+++ Resources/public/js/alloyeditor/buttons/mixins/embeddiscovercontent.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-alloyeditor-button-mixin-embeddiscovercontent', function (Y) {
+    "use strict";
+
+    /**
+     * Provides the EmbedDiscoverContent mixin
+     *
+     * @module ez-alloyeditor-button-mixin-embeddiscovercontent
+     */
+    Y.namespace('eZ.AlloyEditorButton');
+
+    var React = Y.eZ.React;
+
+    /**
+     * ButtonEmbedDiscoverContent is a mixing providing the necessary code to
+     * trigger UDW and use the content choosen by the user to correctly set the
+     * properties of the ezembed widget.
+     *
+     * @class ButtonEmbedDiscoverContent
+     * @namespace eZ.AlloyEditorButton
+     */
+    Y.eZ.AlloyEditorButton.ButtonEmbedDiscoverContent = {
+        propTypes: {
+            /**
+             * The editor instance where the component is being used.
+             *
+             * @property {Object} editor
+             */
+            editor: React.PropTypes.object.isRequired,
+
+            /**
+             * The title of the UDW
+             *
+             * @property {String} udwTitle
+             */
+            udwTitle: React.PropTypes.string,
+
+            /**
+             * The method to use as the contentDiscoveredHandler in the UDW
+             *
+             * @property {String} udwContentDiscoveredMethod
+             */
+            udwContentDiscoveredMethod: React.PropTypes.string,
+        },
+
+        /**
+         * Triggers the UDW to choose the content to embed.
+         *
+         * @method _chooseContent
+         * @protected
+         */
+        _chooseContent: function () {
+            this.props.editor.get('nativeEditor').fire('contentDiscover', {
+                config: {
+                    title: this.props.udwTitle,
+                    multiple: false,
+                    contentDiscoveredHandler: this[this.props.udwContentDiscoveredMethod],
+                }
+            });
+        },
+
+        /**
+         * Sets the href and the content of the ezembed widget based on the
+         * given content
+         *
+         * @method _setContent
+         * @protected
+         * @param {eZ.Content} content
+         */
+        _setContent: function (content) {
+            var editor = this.props.editor.get('nativeEditor'),
+                wrapper, embedWidget;
+
+            wrapper = editor.getSelection().getStartElement();
+            embedWidget = editor.widgets.getByElement(wrapper);
+            embedWidget.element.data(
+                'href',
+                'ezcontent://' + content.get('contentId')
+            );
+            embedWidget.element.setText(content.get('name'));
+        },
+    };
+});
--- Tests/js/alloyeditor/buttons/assets/ez-alloyeditor-button-embed-tests.jsx
+++ Tests/js/alloyeditor/buttons/assets/ez-alloyeditor-button-embed-tests.jsx
@@ -2,6 +2,7 @@
  * Copyright (C) eZ Systems AS. All rights reserved.
  * For full copyright and license information view LICENSE file distributed with this source code.
  */
+/* global CKEDITOR */
 YUI.add('ez-alloyeditor-button-embed-tests', function (Y) {
     var renderTest, clickTest,
         AlloyEditor = Y.eZ.AlloyEditor,
@@ -43,60 +44,108 @@ YUI.add('ez-alloyeditor-button-embed-tests', function (Y) {
     clickTest= new Y.Test.Case({
         name: "eZ AlloyEditor embed button click test",
 
-        setUp: function () {
-            var nat = new Mock(),
-                selection = new Mock(),
-                ezembed = {};
+        "async:init": function () {
+            var startTest = this.callback();
 
+            CKEDITOR.plugins.addExternal('lineutils', '../../../lineutils/');
+            CKEDITOR.plugins.addExternal('widget', '../../../widget/');
             this.container = Y.one('.container');
-            this.editor = new Mock();
-            Mock.expect(this.editor, {
-                method: 'get',
-                args: ['nativeEditor'],
-                returns: nat,
-            });
-            Mock.expect(nat, {
-                method: 'execCommand',
-                args: ['eZAddContent', Mock.Value.Object],
-                run: function (command, data) {
-                    Assert.areEqual(
-                        data.tagName, 'ezembed',
-                        "A ezembed element should have been generated"
-                    );
+            this.editor = AlloyEditor.editable(
+                Y.one('.editorContainer').getDOMNode(), {
+                    extraPlugins: AlloyEditor.Core.ATTRS.extraPlugins.value + ',ezembed',
                 }
+            );
+            this.editor.get('nativeEditor').on('instanceReady', function () {
+                startTest();
             });
-            Mock.expect(nat, {
-                method: 'selectionChange',
-                args: [true],
-            });
-            Mock.expect(nat, {
-                method: 'fire',
-                args: ['actionPerformed', Mock.Value.Object],
-            });
-            nat.widgets = new Mock();
-            Mock.expect(nat, {
-                method: 'getSelection',
-                returns: selection,
-            });
-            Mock.expect(selection, {
-                method: 'getStartElement',
-                returns: ezembed
-            });
-            Mock.expect(nat.widgets, {
-                method: 'initOn',
-                args: [ezembed, 'ezembed'],
-            });
-            this.widgets = nat.widgets;
+        },
+
+        destroy: function () {
+            this.editor.destroy();
         },
 
         tearDown: function () {
             React.unmountComponentAtNode(this.container.getDOMNode());
-            delete this.editor;
         },
 
-        "Should execute the eZAddContent command": function () {
-            var button;
+        "Should fire the contentDiscover event": function () {
+            var button,
+                contentDiscoverFired = false;
 
+            this.editor.get('nativeEditor').on('contentDiscover', function (evt) {
+                contentDiscoverFired = true;
+
+                Assert.isObject(
+                    evt.data.config,
+                    "The event should provide a config for the UDW"
+                );
+                Assert.isFalse(
+                    evt.data.config.multiple,
+                    "The UDW should be configured with multiple false"
+                );
+                Assert.isFunction(
+                    evt.data.config.contentDiscoveredHandler,
+                    "The UDW should be configured with a contentDiscoveredHandler"
+                );
+            });
+            button = React.render(
+                <AlloyEditor.ButtonEmbed editor={this.editor} />,
+                this.container.getDOMNode()
+            );
+
+            this.container.one('button').simulate('click');
+
+            Assert.isTrue(
+                contentDiscoverFired,
+                "The contentDiscover event should have been fired"
+            );
+        },
+
+        "Should add the embed after choosing the content": function () {
+            var button, content = new Mock(),
+                contentName = 'I Am the Highway',
+                contentId = 42;
+
+            Mock.expect(content, {
+                method: 'get',
+                args: [Mock.Value.String],
+                run: function (attr) {
+                    if ( attr === 'contentId' ) {
+                        return contentId;
+                    } else if ( attr === 'name' ) {
+                        return contentName;
+                    }
+                    Assert.fail("Unexpected call to get for attribute " + attr);
+                }
+            });
+
+            this.editor.get('nativeEditor').on('contentDiscover', function (evt) {
+                var wrapper, widget;
+
+                evt.data.config.contentDiscoveredHandler.call(this, {
+                    selection: {
+                        content: content,
+                    },
+                });
+
+                wrapper = this.element.findOne('.cke_widget_element');
+                widget = this.widgets.getByElement(wrapper);
+
+                Assert.areEqual(
+                    'ezembed', widget.name,
+                    "An ezembed widget should have been added"
+                );
+                Assert.areEqual(
+                    'ezcontent://' + contentId,
+                    widget.element.data('href'),
+                    "The data-href should be build with the contentId"
+                );
+                Assert.areEqual(
+                    contentName,
+                    widget.element.getText(),
+                    "The ezembed should be filled with the content name"
+                );
+            });
             button = React.render(
                 <AlloyEditor.ButtonEmbed editor={this.editor} />,
                 this.container.getDOMNode()
@@ -104,15 +153,9 @@ YUI.add('ez-alloyeditor-button-embed-tests', function (Y) {
 
             this.container.one('button').simulate('click');
         },
-
-        "Should initialize the widget on the ezembed element": function () {
-            this["Should execute the eZAddContent command"]();
-
-            Mock.verify(this.widgets);
-        },
     });
 
     Y.Test.Runner.setName("eZ AlloyEditor embed button tests");
     Y.Test.Runner.add(renderTest);
     Y.Test.Runner.add(clickTest);
-}, '', {requires: ['test', 'node', 'node-event-simulate', 'ez-alloyeditor-button-embed']});
+}, '', {requires: ['test', 'node', 'node-event-simulate', 'ez-alloyeditor-button-embed', 'ez-alloyeditor-plugin-embed']});
