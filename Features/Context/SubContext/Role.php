a6664e5fcd05fe3f7584855e430f1eb4dd9a85e9
Miguel <miguel.jacinto@cleverti.com>
2015-10-02 14:13:43 +0100

    Added Role UI BDD feature file and implementations

---
+++ Features/Context/SubContext/Role.php
@@ -0,0 +1,210 @@
+<?php
+
+/**
+ * File containing the Common Functions for context class PlatformUI.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace EzSystems\PlatformUIBundle\Features\Context\SubContext;
+
+use Behat\Gherkin\Node\TableNode;
+
+trait Role
+{
+    /**
+     * @Given I am on the RolesUI
+     */
+    public function onRolesPage()
+    {
+        $this->visit('/ez#/admin/pjax%2Frole');
+    }
+
+    /**
+     * @Given I create a new role
+     */
+    public function createRole()
+    {
+        $this->clickElementByText('Create a role', '.ez-button');
+    }
+
+    /**
+     * @When I click on the Roles
+     */
+    public function clickRoles()
+    {
+        $this->clickElementByText('Roles', 'li a');
+    }
+
+    /**
+     * @When I edit the :name role
+     */
+    public function iEditRole($name)
+    {
+        $page = $this->getSession()->getPage();
+        $elements = $page->findAll('css', '.ez-role');
+        if (!$elements) {
+            throw new \Exception('No roles found');
+        }
+        foreach ($elements as $element) {
+            $foundName = $this->getElementByText($name, '.ez-role-name', null, $element);
+            if ($foundName) {
+                $this->getElementByText('Edit', '.ez-role-edit a', null, $element)->click();
+                break;
+            }
+        }
+        if (!$foundName) {
+            throw new \Exception("Role $name not found");
+        }
+    }
+
+    /**
+     * @When I am viewing the :role role's details
+     */
+    public function roleDetailsView($role)
+    {
+        $this->onRolesPage();
+        $this->waitForLoadings();
+        $this->clickElementByText($role, '.ez-role-name a');
+    }
+
+    /**
+     * @Then I see the Roles page
+     */
+    public function iSeeRolePage()
+    {
+        $this->iSeeTitle('Roles');
+    }
+
+    /**
+     * @When I delete the :name role
+     */
+    public function deleteRole($name)
+    {
+        $this->clickElementByText($name, '.ez-role-name a');
+        $this->waitForLoadings();
+        $this->clickElementByText('Delete', '.ez-button');
+    }
+
+    /**
+     * @Then I should see that there are no policies set up for this role
+     */
+    public function noPoliciesForThisRole()
+    {
+        $this->getElementByText('There are no policies set up for this role.', 'tr td');
+    }
+
+    /**
+     * @Then I should see that this role has no Role Assigments
+     */
+    public function noAssigmentsForThisRole()
+    {
+        $this->getElementByText('This role is not assigned to any users or user groups.', 'tr td');
+    }
+
+    /**
+     * @Then the Role is successfully published
+     */
+    public function roleWasPublished()
+    {
+        $this->iSeeNotification('The role was published.');
+    }
+
+    /**
+     * @Then the Role :name is not published
+     * @Then the Role :name is successfully deleted
+     */
+    public function roleWasNotPublished($name)
+    {
+        $element = $this->getElementByText($name, '.ez-role-name');
+        if ($element) {
+            throw new \Exception('Role was found');
+        }
+    }
+
+    /**
+     * @Then I see a message saying that the name :name already exists
+     */
+    public function nameAlreadyExists($name)
+    {
+        $this->iSeeNotification('Form did not validate. Please review errors below.');
+        $element = $this->getElementByText(
+            'Identifier "' .  $name . '" already exists. Role identifier must be unique.',
+            'li'
+        );
+        if (!$element) {
+            throw new \Exception('Error message not found');
+        }
+    }
+
+    /**
+     * @Then I should see a label for the Role Assigments
+     */
+    public function roleAssigmentLabel()
+    {
+        $this->iSeeTitle('Invalid argument: The role name must be unique.');
+    }
+
+    /**
+     * @Then I see a message asking for the field :label to be filled
+     */
+    public function labelMustBeFilled($label)
+    {
+        //Empty for now, while the notification system is not working for the empty fields
+    }
+
+    /**
+     * @Then I see the following roles with an :button button:
+     */
+    public function iSeeRolesList(TableNode $roles, $button)
+    {
+        $page = $this->getSession()->getPage();
+        $elements = $page->findAll('css', '.ez-role');
+        if (!$elements) {
+            throw new \Exception('No roles found');
+        }
+        foreach ($roles as $role) {
+            $name = $role['Name'];
+            foreach ($elements as $element) {
+                $foundName = $this->getElementByText($name, '.ez-role-name', null, $element);
+                $foundButton = $this->getElementByText($button, '.ez-role-edit', null, $element);
+                if ($foundName) {
+                    break;
+                }
+            }
+            if (!$foundName) {
+                throw new \Exception("Role $name not found");
+            }
+            if (!$foundButton) {
+                throw new \Exception("Role $name does not have a $button button");
+            }
+        }
+    }
+
+    /**
+     * @Then I should see a/an :label button
+     */
+    public function iSeeButton($label)
+    {
+        $element = $this->getElementByText($label, '.ez-button');
+        if (!$element) {
+            throw new \Exception("Button with label $label not found");
+        }
+    }
+
+    /**
+     * @Then I see the following limitations fields:
+     * @Then I should see a group with the Role Assigments:
+     */
+    public function iSeeLimitationFields(TableNode $limitations)
+    {
+        $limitations = $limitations->getRow(0);
+        foreach ($limitations as $limitation) {
+            $element = $this->getElementByText($limitation, '.ez-selection-table th');
+            if (!$element) {
+                throw new \Exception("Limitation $limitation not found");
+            }
+        }
+    }
+}
---
+++ Features/RoleUI/RoleUI.feature
@@ -0,0 +1,174 @@
+Feature: Create, delete, update and View the Role UI
+  In order to create, delete, update and view the ROLE UI
+  As an Administrator user
+  I need to be able to access and navigate through the Role UI interface
+
+  Background:
+    Given I am logged in as admin on PlatformUI
+
+  ##
+  #
+  # Validation Roles fields
+  #
+  ##
+  @javascript @common
+  Scenario: Verify the existence of Roles page and its content
+    Given an "Organizers" role exists
+    When I click on the navigation zone "Admin Panel"
+    When I click on the navigation item "Administration dashboard"
+    And I click on the "Roles" link
+    Then I see the Roles page
+    And I see the following roles with an 'Edit' button:
+      | Name          |
+      | Anonymous     |
+      | Administrator |
+      | Editor        |
+      | Partner       |
+      | Member        |
+      | Organizers    |
+    And I should see a "Create a role" button
+
+  @javascript @common
+  Scenario: Verify the information displayed by one of the roles
+    Given I am on the Roles page
+    When I click on the "Anonymous" link
+    Then I see the following limitations fields:
+      | Module | Function | Limitation |
+    And I should see an "Edit" button
+    And I should see a "Delete" button
+    And I should see a group with the Role Assigments:
+      | User/group | Limitation |
+
+  @javascript @edge
+  Scenario: Display the information of a role with empty policies
+    Given an "Organizers" role exists
+    And "Organizers" do not have any assigned policies
+    And "Organizers" do not have any assigned Users and groups
+    When I am viewing the "Organizers" role's details
+    Then I should see that there are no policies set up for this role
+    And I should see that this role has no Role Assigments
+
+  ##
+  #
+  # Creating Roles
+  #
+  ##
+  @javascript @common
+  Scenario: Validate the existence of expected fields when creating a role
+    Given I am on the Roles page
+    When I create a new role
+    Then I should see a "Name" input field
+    And I should see a "Save" button
+    And I should see a "Cancel" button
+
+  @javascript @common
+  Scenario: Create a valid role
+    Given I am on the Roles page
+    And I create a new role
+    And I fill in "Name" with "Organizers"
+    When I click on "Save" button
+    Then the Role is successfully published
+    And I see that an "Organizers" role exists
+
+  @javascript @edge
+  Scenario: create a valid role with japanese characters
+    Given I am on the Roles page
+    And I create a new role
+    And I fill in "Name" with "私は日本の名前で試してみる必要が"
+    When I click on "Save" button
+    Then the Role is successfully published
+    And I see that an "私は日本の名前で試してみる必要が" role exists
+
+  @javascript @edge
+  Scenario: creating a Role with an empty name triggers an obligation to fill the name
+    Given I am on the Roles page
+    And I create a new role
+    And I set "Name" as empty
+    When I click on "Save" button
+    Then I see a message asking for the field "Name" to be filled
+
+  @javascript @edge
+  Scenario: creating a Role with an existing identifier fails validation
+    Given an "Organizers" role exists
+    And I am on the Roles page
+    And I create a new role
+    And I fill in "Name" with "Organizers"
+    When I click on "Save" button
+    Then I see a message saying that the name "Organizers" already exists
+    And the Role "Organizers" is not published
+
+  @javascript @common
+  Scenario: cancel the creation of a role
+    Given I am on the Roles page
+    And I create a new role
+    And I fill in "Name" with "Organizer"
+    When I click on "Cancel" button
+    Then the Role "Organizer" is not published
+    And I see that an "Organizer" role does not exists
+
+  ##
+  #
+  # Updating Roles
+  #
+  ##
+  @javascript @common
+  Scenario: Validate the existence of expected fields when editing a role
+    Given an "Organizers" role exists
+    And I am on the Roles page
+    When I edit the "Organizers" role
+    Then I should see "Edit <Organizers>" title
+    And I should see a "Name" input field
+    And I should see a "Save" button
+    And I should see a "Cancel" button
+
+  @javascript @common
+  Scenario: Update a role
+    Given an "Organizers" role exists
+    And I am on the Roles page
+    And I edit the "Organizers" role
+    And I fill in "Name" with "old_Organizers"
+    When I click on "Save" button
+    Then the Role is successfully published
+    And I see that an "old_Organizers" role exists
+
+  @javascript @edge
+  Scenario: Updating a Role with an empty name triggers a message asking to fill the name
+    Given an "Organizers" role exists
+    And I am on the Roles page
+    And I edit the "Organizers" role
+    And I set "Name" as empty
+    When I click on "Save" button
+    Then I see a message asking for the field "Name" to be filled
+
+  @javascript @edge
+  Scenario: Updating a Role with an existing identifier fails validation
+    Given an "Organizers" role exists
+    And a "Security" role exists
+    And I am on the Roles page
+    And I edit the "Organizers" role
+    And I fill in "Name" with "Security"
+    When I click on "Save" button
+    Then I see a message saying that the name "Security" already exists
+
+  @javascript @common
+  Scenario: Cancel the update of a role
+    Given an "Organizers" role exists
+    And I am on the Roles page
+    And I edit the "Organizers" role
+    And I fill in "Name" with "Security"
+    When I click on "Cancel" button
+    Then the Role "Security" is not published
+    And I see that an "Security" role does not exists
+
+  ##
+  #
+  # Deleting Roles
+  #
+  ##
+  @javascript @common
+  Scenario: Delete a role
+    Given an "Organizers" role exists
+    And I am on the Roles page
+    When I delete the "Organizers" role
+    Then the Role "Organizers" is successfully deleted
+    And I see that an "Organizers" role does not exists
