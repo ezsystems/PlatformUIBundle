5f5b66ff4e3700c41af6fbba2e9c9078c9d8f2b5
Damien Pobel <dp@ez.no>
2015-10-07 11:16:23 +0200

    EZP-24894: Adapt the eZRemoveBlock command to handle the embed widget

--- Resources/public/js/alloyeditor/plugins/removeblock.js
+++ Resources/public/js/alloyeditor/plugins/removeblock.js
@@ -13,14 +13,84 @@ YUI.add('ez-alloyeditor-plugin-removeblock', function (Y) {
     }
 
     removeBlockCommand = {
+        /**
+         * Moves the caret to the element
+         *
+         * @method _moveCaretToElement
+         * @protected
+         * @param {CKEDITOR.editor} editor
+         * @param {CKEDITOR.dom.element} element
+         */
+        _moveCaretToElement: function (editor, element) {
+            var range = editor.createRange();
+
+            range.moveToPosition(element, CKEDITOR.POSITION_AFTER_START);
+            editor.getSelection().selectRanges([range]);
+        },
+
+        /**
+         * Fires the editorInteraction event so that AlloyEditor's UI is updated
+         * for the newly focused element
+         *
+         * @method _fireEditorInteraction
+         * @protected
+         * @param {CKEDITOR.editor} editor
+         * @param {CKEDITOR.dom.element} removedElement
+         * @param {CKEDITOR.dom.element} newFocus
+         */
+        _fireEditorInteraction: function (editor, newFocus) {
+            var e = {
+                    editor: editor,
+                    target: newFocus.$,
+                    name: 'eZRemoveBlockDone',
+                };
+
+            editor.fire('editorInteraction', {
+                nativeEvent: e,
+                selectionData: editor.getSelectionData(),
+            });
+
+        },
+
+        /**
+         * Changes the focused element in the editor to the given newFocus
+         * element
+         *
+         * @param {CKEDITOR.editor} editor
+         * @param {CKEDITOR.dom.element} newFocus
+         * @protected
+         * @method _changeFocus
+         */
+        _changeFocus: function (editor, newFocus) {
+            this._moveCaretToElement(editor, newFocus);
+            this._fireEditorInteraction(editor, newFocus);
+       },
+
         exec: function (editor, data) {
-            editor.elementPath().block.remove();
+            var toRemove = editor.elementPath().block,
+                newFocus;
+
+            if ( !toRemove ) {
+                // path.block is null when a widget is focused so the element to
+                // remove is the focused widget wrapper.
+                toRemove = editor.widgets.focused.wrapper;
+            }
+            newFocus = toRemove.getNext();
+            if ( !newFocus ) {
+                newFocus = toRemove.getPrevious();
+            }
+
+            toRemove.remove();
+            if ( newFocus ) {
+                this._changeFocus(editor, newFocus);
+            }
         },
     };
 
     /**
      * CKEditor plugin providing the eZRemoveBlock command. This command
-     * allows to remove the block element holding the caret.
+     * allows to remove the block element holding the caret or the focused
+     * widget
      *
      * @class CKEDITOR.plugins.ezremoveblock
      * @constructor
--- Tests/js/alloyeditor/plugins/assets/ez-alloyeditor-plugin-removeblock-tests.js
+++ Tests/js/alloyeditor/plugins/assets/ez-alloyeditor-plugin-removeblock-tests.js
@@ -2,9 +2,9 @@
  * Copyright (C) eZ Systems AS. All rights reserved.
  * For full copyright and license information view LICENSE file distributed with this source code.
  */
-/* global CKEDITOR */
+/* global CKEDITOR, AlloyEditor */
 YUI.add('ez-alloyeditor-plugin-removeblock-tests', function (Y) {
-    var definePluginTest, commandTest,
+    var definePluginTest, removeTest, focusTest,
         Assert = Y.Assert, Mock = Y.Mock;
 
     definePluginTest = new Y.Test.Case({
@@ -44,38 +44,168 @@ YUI.add('ez-alloyeditor-plugin-removeblock-tests', function (Y) {
         },
     });
 
-    commandTest = new Y.Test.Case({
-        name: "eZ AlloyEditor removeblock plugin command test",
+    removeTest = new Y.Test.Case({
+        name: "eZ AlloyEditor eZRemoveBlock command test",
 
-        setUp: function () {
-            var editor = {
-                    addCommand: Y.bind(function (name, command) {
-                        this.command = command;
-                    }, this)
-                };
-            CKEDITOR.plugins.get('ezremoveblock').init(editor);
+        "async:init": function () {
+            var startTest = this.callback();
+
+            CKEDITOR.plugins.addExternal('lineutils', '../../../lineutils/');
+            CKEDITOR.plugins.addExternal('widget', '../../../widget/');
+            this.container = Y.one('.container');
+            this.editor = AlloyEditor.editable(
+                this.container.getDOMNode(), {
+                    extraPlugins: AlloyEditor.Core.ATTRS.extraPlugins.value + ',ezremoveblock,widget,ezembed',
+                }
+            );
+            this.editor.get('nativeEditor').on('instanceReady', function () {
+                startTest();
+            });
+        },
+
+        destroy: function () {
+            this.editor.destroy();
+            this.container.set('innerHTML', '');
+        },
+
+        _removeElement: function (html, id) {
+            var editor = this.editor.get('nativeEditor'),
+                selection = editor.getSelection();
+
+            editor.setData(html);
+            selection.selectElement(editor.element.findOne(id));
+            editor.execCommand('eZRemoveBlock');
+        },
+
+        _removeWidget: function (html, id) {
+            var editor = this.editor.get('nativeEditor');
+
+            editor.setData(html);
+            editor.widgets.getByElement(editor.element.findOne(id)).focus();
+            editor.execCommand('eZRemoveBlock');
         },
 
         "Should remove the current block element": function () {
-            var editor = new Mock(),
-                block = new Mock();
+            this._removeElement(
+                '<p id="remove-me">Remove me</p>',
+                '#remove-me'
+            );
+            Assert.isNull(
+                Y.one('#remove-me'),
+                "The selected element should have been removed"
+            );
+        },
 
-            Mock.expect(editor, {
-                method: 'elementPath',
-                returns: {
-                    block: block
+        "Should remove the widget element": function () {
+            this._removeWidget(
+                '<div data-ezelement="embed" id="remove-me">Remove me</div>',
+                '#remove-me'
+            );
+            Assert.isNull(
+                Y.one('#remove-me'),
+                "The widget should have been removed"
+            );
+        },
+    });
+
+    focusTest= new Y.Test.Case({
+        name: "eZ AlloyEditor eZRemoveBlock command focus change test test",
+
+        "async:init": function () {
+            var startTest = this.callback();
+
+            this.container = Y.one('.container');
+            this.editor = AlloyEditor.editable(
+                this.container.getDOMNode(), {
+                    extraPlugins: AlloyEditor.Core.ATTRS.extraPlugins.value + ',ezremoveblock',
                 }
+            );
+            this.editor.get('nativeEditor').on('instanceReady', function () {
+                startTest();
             });
-            Mock.expect(block, {
-                method: 'remove',
-            });
+        },
 
-            this.command.exec(editor);
-            Mock.verify(block);
+        destroy: function () {
+            this.editor.destroy();
+            this.container.set('innerHTML', '');
+        },
+
+        _removeElement: function (html, id) {
+            var editor = this.editor.get('nativeEditor'),
+                selection = editor.getSelection();
+
+            editor.setData(html);
+            selection.selectElement(editor.element.findOne(id));
+            editor.execCommand('eZRemoveBlock');
+        },
+
+        "Should give the focus to the next sibling element": function () {
+            var editor = this.editor.get('nativeEditor'),
+                editorInteractionFired = false;
+
+            editor.once('editorInteraction', function (evt) {
+                var natEvt = evt.data.nativeEvent,
+                    remaining = Y.one('#remaining').getDOMNode();
+
+                editorInteractionFired = true;
+                Assert.areSame(
+                    editor, natEvt.editor,
+                    "The editor should be provided in the native event"
+                );
+                Assert.areSame(
+                    remaining, natEvt.target,
+                    "The target should be the newly focused element"
+                );
+                Assert.areEqual(
+                    remaining, editor.getSelection().getStartElement().$,
+                    "The next sibling element should have been selected"
+                );
+            });
+            this._removeElement(
+                '<p id="remove-me">Remove me</p><p id="remaining">Next remaining</p>',
+                '#remove-me'
+            );
+            Assert.isTrue(
+                editorInteractionFired,
+                "The `editorInteraction` event should have been fired"
+            );
+        },
+
+        "Should give the focus to the previous sibling": function () {
+            var editor = this.editor.get('nativeEditor'),
+                editorInteractionFired = false;
+
+            editor.once('editorInteraction', function (evt) {
+                var natEvt = evt.data.nativeEvent,
+                    remaining = Y.one('#remaining').getDOMNode();
+
+                editorInteractionFired = true;
+                Assert.areSame(
+                    editor, natEvt.editor,
+                    "The editor should be provided in the native event"
+                );
+                Assert.areSame(
+                    remaining, natEvt.target,
+                    "The target should be the newly focused element"
+                );
+                Assert.areEqual(
+                    remaining, editor.getSelection().getStartElement().$,
+                    "The next sibling element should have been selected"
+                );
+            });
+            this._removeElement(
+                '<p id="remaining">Next remaining</p><p id="remove-me">Remove me</p>',
+                '#remove-me'
+            );
+            Assert.isTrue(
+                editorInteractionFired,
+                "The `editorInteraction` event should have been fired"
+            );
         },
     });
 
     Y.Test.Runner.setName("eZ AlloyEditor removeblock plugin tests");
     Y.Test.Runner.add(definePluginTest);
-    Y.Test.Runner.add(commandTest);
-}, '', {requires: ['test', 'node', 'ez-alloyeditor-plugin-removeblock']});
+    Y.Test.Runner.add(removeTest);
+    Y.Test.Runner.add(focusTest);
+}, '', {requires: ['test', 'node', 'ez-alloyeditor-plugin-removeblock', 'ez-alloyeditor-plugin-embed']});
