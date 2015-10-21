0b60501cc779fcd78c20767d4a9a94a98c8e4c6d
André R <ar@ez.no>
2015-10-21 23:53:29 +0200

    [UNITTEST] Skip boostrap if autoloading is handled

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,6 +4,12 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+
+// Skip autoloading if already done by phpunit alias (including from meta repo if this is vendor)
+if (defined('PHPUNIT_COMPOSER_INSTALL')) {
+    return;
+}
+
 $autoloadFile = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($autoloadFile)) {
     throw new RuntimeException('Install dependencies to run test suite.');
