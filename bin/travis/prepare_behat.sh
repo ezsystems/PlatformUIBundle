d21acf2935c1166910a9ea31f5db1e6f80dee982
Damien Pobel <dp@ez.no>
2016-03-07 14:14:02 +0100

    Reused ezplatform script

--- bin/travis/prepare_behat.sh
+++ bin/travis/prepare_behat.sh
@@ -10,25 +10,10 @@ cd "$HOME/build"
 git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezplatform.git
 cd ezplatform
 
-##
-# from  ./bin/.travis/prepare_system.sh
-##
-echo "> Disable xdebug";
-phpenv config-rm xdebug.ini ;
-
-
-echo "> Create database and grant premissions to user 'ezp'"
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS behattestdb; GRANT ALL ON behattestdb.* TO ezp@localhost IDENTIFIED BY 'ezp';"
-
-##
-# end ./bin/.travis/prepare_system.sh
-##
-
+./bin/.travis/disable_xdebug.sh
+./bin/.travis/configure_mysql.sh
 ./bin/.travis/prepare_selenium2.sh
 
-##
-# from ./bin/.travis/setup_from_external_repo.sh
-##
 echo "> Modify composer.json to point to local checkout"
 sed -i '$d' composer.json
 echo ',    "repositories": [{"type":"git","url":"'$TRAVIS_BUILD_DIR'"}]}' >> composer.json
@@ -36,32 +21,7 @@ echo ',    "repositories": [{"type":"git","url":"'$TRAVIS_BUILD_DIR'"}]}' >> com
 echo "> Updating packages (ezsystems/platform-ui-bundle:dev-tmp_travis_branch as 1.0)"
 composer require --no-update "ezsystems/platform-ui-bundle:dev-tmp_travis_branch as 1.0"
 
-#cat composer.json
-
-##
-# from ./bin/.travis/prepare_ezpublish.sh
-##
-echo "> Setup github auth key to not reach api limit"
-cp bin/.travis/composer-auth.json ~/.composer/auth.json
-
-echo "> Copy behat specific parameters.yml settings"
-cp bin/.travis/parameters.yml app/config/
-
-## Switch to another Symfony version if asked for (with composer update to not use composer.lock if present)
-#if [ "$SYMFONY_VERSION" != "" ] ; then
-#    echo "> Install dependencies through Composer (with custom Symfony version: ${SYMFONY_VERSION})"
-#    composer require --no-update symfony/symfony="${SYMFONY_VERSION}"
-#    composer update --no-progress --no-interaction --prefer-dist
-#else
-    echo "> Install dependencies through Composer"
-    composer install --no-progress --no-interaction --prefer-dist
-#fi
-
-echo "> Run assetic dump for behat env"
-php app/console --env=behat --no-debug assetic:dump
-
-echo "> Installing ezplatform clean"
-php app/console --env=behat ezplatform:install clean
+./bin/.travis/prepare_ezpublish.sh
 
 echo "> Running the server"
 php app/console server:start --router=bin/.travis/router_behat.php --env=behat
