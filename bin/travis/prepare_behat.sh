cd771418f5952d161f2a732012e1cd5c8c864fa7
Bertrand Dunogier <bd@ez.no>
2016-05-12 14:19:06 +0200

    [tmp] set custom ezplatform branch

--- bin/travis/prepare_behat.sh
+++ bin/travis/prepare_behat.sh
@@ -7,7 +7,7 @@ git fetch --unshallow && git checkout -b tmp_travis_branch
 cd "$HOME/build"
 
 # Checkout meta repo, change the branch and/or remote to use a different ezpublish branch/distro
-git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezplatform.git
+git clone --depth 1 --single-branch --branch behat_disable_in_memory_cache https://github.com/ezsystems/ezplatform.git
 cd ezplatform
 
 ./bin/.travis/disable_xdebug.sh
