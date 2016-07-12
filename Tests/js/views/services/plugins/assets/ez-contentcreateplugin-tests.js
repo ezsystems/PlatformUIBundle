e1c7787c225c0c6e109476039446f9276026f164
Damien Pobel <dp@ez.no>
2016-07-12 12:13:37 +0200

    EZP-26049: Moved the all content types loading logic to a dedicated plugin

--- Tests/js/views/services/plugins/assets/ez-contentcreateplugin-tests.js
+++ Tests/js/views/services/plugins/assets/ez-contentcreateplugin-tests.js
@@ -4,123 +4,31 @@
  */
 YUI.add('ez-contentcreateplugin-tests', function (Y) {
     var createContentActionEvent, registerTest, createContentEvent,
-        loadTest, nextServiceTest,
+        loadTest, nextServiceTest, registerContentTypeTest,
         Assert = Y.Assert, Mock = Y.Mock;
 
     createContentActionEvent = new Y.Test.Case({
         name: 'eZ Create Content plugin create content action event tests',
 
         setUp: function () {
-            this.defaultlanguageCode = 'eng-GB';
+            var defaultLanguageCode = 'eng-GB';
 
-            this.appMock =  new Y.Test.Mock();
-            this.capiMock = new Y.Test.Mock();
-            this.contentTypeServiceMock = new Y.Test.Mock();
-
+            this.appMock = new Y.Test.Mock();
             Y.Mock.expect(this.appMock, {
                 method: 'get',
                 args: ['contentCreationDefaultLanguageCode'],
-                returns: this.defaultLanguageCode,
+                returns: defaultLanguageCode,
             });
-
-            Y.Mock.expect(this.capiMock, {
-                method: 'getContentTypeService',
-                returns: this.contentTypeServiceMock,
-            });
-
             this.service = new Y.eZ.ViewService({
-                capi: this.capiMock,
                 app: this.appMock,
             });
+            this.service.contentType = new Mock();
 
             this.view = new Y.View();
             this.view.set('loadingError', false);
-            this.plugin = new Y.eZ.Plugin.ContentCreate({host: this.service});
-
             this.view.addTarget(this.service);
 
-            this.responseGroups = {
-                "ContentTypeGroupList": {
-                    "_media-type": "application\/vnd.ez.api.ContentTypeGroupList+json",
-                    "_href": "\/api\/ezp\/v2\/content\/typegroups",
-                    "ContentTypeGroup": [
-                        {
-                            "_media-type": "application\/vnd.ez.api.ContentTypeGroup+json",
-                            "_href": "\/api\/ezp\/v2\/content\/typegroups\/1",
-                            "id": 1,
-                            "identifier": "Content",
-                            "created": "2002-09-05T11:08:48+02:00",
-                            "modified": "2002-10-06T18:35:06+02:00",
-                            "ContentTypes": {
-                                "_media-type": "application\/vnd.ez.api.ContentTypeInfoList+json",
-                                "_href": "\/api\/ezp\/v2\/content\/typegroups\/1\/types"
-                            }
-                        },
-                        {
-                            "_media-type": "application\/vnd.ez.api.ContentTypeGroup+json",
-                            "_href": "\/api\/ezp\/v2\/content\/typegroups\/2",
-                            "id": 2,
-                            "identifier": "Users",
-                            "created": "2002-09-05T11:09:01+02:00",
-                            "modified": "2002-10-06T18:35:13+02:00",
-                            "ContentTypes": {
-                                "_media-type": "application\/vnd.ez.api.ContentTypeInfoList+json",
-                                "_href": "\/api\/ezp\/v2\/content\/typegroups\/2\/types"
-                            }
-                        }
-                    ]
-                }
-            };
-            this.responseTypes = {
-                "ContentTypeInfoList": {
-                    "_media-type": "application\/vnd.ez.api.ContentTypeInfoList+json",
-                    "_href": "\/api\/ezp\/v2\/content\/typegroups\/2\/types",
-                    "ContentType": [
-                        {
-                            "_media-type": "application\/vnd.ez.api.ContentTypeInfo+json",
-                            "_href": "\/api\/ezp\/v2\/content\/types\/3",
-                            "id": 3,
-                            "status": "DEFINED",
-                            "identifier": "user_group",
-                            "names": {
-                                "value": [
-                                    {
-                                        "_languageCode": "eng-GB",
-                                        "#text": "User group"
-                                    }
-                                ]
-                            },
-                            "creationDate": "2002-06-18T11:21:38+02:00",
-                            "modificationDate": "2003-03-24T09:32:23+01:00",
-                            "Creator": {
-                                "_media-type": "application\/vnd.ez.api.User+json",
-                                "_href": "\/api\/ezp\/v2\/user\/users\/14"
-                            },
-                            "Modifier": {
-                                "_media-type": "application\/vnd.ez.api.User+json",
-                                "_href": "\/api\/ezp\/v2\/user\/users\/14"
-                            },
-                            "Groups": {
-                                "_media-type": "application\/vnd.ez.api.ContentTypeGroupRefList+json",
-                                "_href": "\/api\/ezp\/v2\/content\/types\/3\/groups"
-                            },
-                            "Draft": {
-                                "_media-type": "application\/vnd.ez.api.ContentType+json",
-                                "_href": "\/api\/ezp\/v2\/content\/types\/3\/draft"
-                            },
-                            "remoteId": "25b4268cdcd01921b808a0d854b877ef",
-                            "urlAliasSchema": null,
-                            "nameSchema": "<name>",
-                            "isContainer": true,
-                            "mainLanguageCode": "eng-GB",
-                            "defaultAlwaysAvailable": true,
-                            "defaultSortField": "PATH",
-                            "defaultSortOrder": "ASC"
-                        }
-                    ]
-                }
-            };
-
+            this.plugin = new Y.eZ.Plugin.ContentCreate({host: this.service});
         },
 
         tearDown: function () {
@@ -130,43 +38,31 @@ YUI.add('ez-contentcreateplugin-tests', function (Y) {
             delete this.service;
             delete this.view;
             delete this.plugin;
-            delete this.contentTypeServiceMock;
-            delete this.capiMock;
-            delete this.responseGroups;
-            delete this.responseTypes;
+            delete this.app;
         },
 
         'Should not load the content types if the view is not expanded': function () {
-            Y.Mock.expect(this.contentTypeServiceMock, {
-                method: 'loadContentTypeGroups',
+            Mock.expect(this.service.contentType, {
+                method: 'loadAllContentTypes',
+                args: [Mock.Value.Function],
                 callCount: 0,
             });
             this.view.set('expanded', false);
             this.view.fire('whatever:createContentAction');
 
-            Y.Mock.verify(this.contentTypeServiceMock);
+            Mock.verify(this.service.contentType);
         },
 
         'Should load the content type groups and content types': function () {
-            var that = this,
-                groups;
+            var loadedGroups = [];
 
-            Y.Mock.expect(this.contentTypeServiceMock, {
-                method: 'loadContentTypeGroups',
-                args: [Y.Mock.Value.Function],
+            Mock.expect(this.service.contentType, {
+                method: 'loadAllContentTypes',
+                args: [Mock.Value.Function],
                 run: function (callback) {
-                    callback(false, {document: that.responseGroups});
+                    callback(false, loadedGroups);
                 },
             });
-
-            Y.Mock.expect(this.contentTypeServiceMock, {
-                method: 'loadContentTypes',
-                args: [Y.Mock.Value.String, Y.Mock.Value.Function],
-                run: function (id, callback) {
-                    callback(false, {document: that.responseTypes});
-                }
-            });
-
             this.view.set('expanded', true);
             this.view.fire('whatever:createContentAction');
 
@@ -174,68 +70,21 @@ YUI.add('ez-contentcreateplugin-tests', function (Y) {
                 this.view.get('loadingError'),
                 "The loading error flag of the view should be set to false"
             );
-            groups = this.view.get('contentTypeGroups');
 
-            Y.Assert.areEqual(
-                this.responseGroups.ContentTypeGroupList.ContentTypeGroup.length,
-                groups.length,
+            Assert.areSame(
+                loadedGroups,
+                this.view.get('contentTypeGroups'),
                 "The groups should be available in the view"
-            );
-
-            Y.Array.each(this.responseGroups.ContentTypeGroupList.ContentTypeGroup, function (groupInfo, i) {
-                Y.Assert.areEqual(
-                    groupInfo._href,
-                    groups[i].get('id'),
-                    "The content type groups should be parsed"
-                );
-                Y.Assert.areEqual(
-                    groupInfo.identifier,
-                    groups[i].get('identifier'),
-                    "The content type groups should be parsed"
-                );
-                Y.Assert.areEqual(
-                    that.responseTypes.ContentTypeInfoList.ContentType.length,
-                    groups[i].get('contentTypes').length,
-                    "THe content types should be loaded"
-                );
-            });
-        },
-
-        'Should handle the error while loading the group': function () {
-            Y.Mock.expect(this.contentTypeServiceMock, {
-                method: 'loadContentTypeGroups',
-                args: [Y.Mock.Value.Function],
-                run: function (callback) {
-                    callback(true);
-                },
-            });
-
-            this.view.set('expanded', true);
-            this.view.fire('whatever:createContentAction');
-
-            Y.Assert.isTrue(
-                this.view.get('loadingError'),
-                "The loading error flag of the view should be set to true"
             );
         },
 
         'Should handle the error while loading the content types': function () {
-            var that = this;
-
-            Y.Mock.expect(this.contentTypeServiceMock, {
-                method: 'loadContentTypeGroups',
-                args: [Y.Mock.Value.Function],
+            Mock.expect(this.service.contentType, {
+                method: 'loadAllContentTypes',
+                args: [Mock.Value.Function],
                 run: function (callback) {
-                    callback(false, {document: that.responseGroups});
-                },
-            });
-
-            Y.Mock.expect(this.contentTypeServiceMock, {
-                method: 'loadContentTypes',
-                args: [Y.Mock.Value.String, Y.Mock.Value.Function],
-                run: function (id, callback) {
                     callback(true);
-                }
+                },
             });
 
             this.view.set('expanded', true);
@@ -444,12 +293,17 @@ YUI.add('ez-contentcreateplugin-tests', function (Y) {
     registerTest.Plugin = Y.eZ.Plugin.ContentCreate;
     registerTest.components = ['locationViewViewService'];
 
+    registerContentTypeTest = new Y.Test.Case(Y.eZ.Test.PluginRegisterTest);
+    registerContentTypeTest.Plugin = Y.eZ.Plugin.ContentType;
+    registerContentTypeTest.components = ['locationViewViewService'];
+
     Y.Test.Runner.setName('eZ Content Create Plugin tests');
     Y.Test.Runner.add(createContentActionEvent);
     Y.Test.Runner.add(createContentEvent);
     Y.Test.Runner.add(loadTest);
     Y.Test.Runner.add(nextServiceTest);
     Y.Test.Runner.add(registerTest);
+    Y.Test.Runner.add(registerContentTypeTest);
 }, '', {
-    requires: ['test', 'base', 'ez-contentcreateplugin', 'ez-pluginregister-tests', 'view']
+    requires: ['test', 'base', 'ez-contentcreateplugin', 'ez-pluginregister-tests', 'view', 'ez-viewservice']
 });
---
+++ Tests/js/views/services/plugins/assets/ez-contenttypeplugin-tests.js
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) eZ Systems AS. All rights reserved.
+ * For full copyright and license information view LICENSE file distributed with this source code.
+ */
+YUI.add('ez-contenttypeplugin-tests', function (Y) {
+    var loadAllContentTypesTest,
+        Assert = Y.Assert, Mock = Y.Mock;
+
+    loadAllContentTypesTest = new Y.Test.Case({
+        name: 'eZ Content Type plugin loadAllContentTypes tests',
+
+        setUp: function () {
+            this.capi = new Mock();
+            this.typeService = new Mock();
+            Mock.expect(this.capi, {
+                method: 'getContentTypeService',
+                returns: this.typeService,
+            });
+            this.service = new Y.eZ.ViewService({
+                capi: this.capi,
+            });
+            this.responseGroups = {
+                document: {
+                    ContentTypeGroupList: {
+                        ContentTypeGroup: [{
+                            '_href': '/1',
+                        }, {
+                            '_href': '/2',
+                        }],
+                    },
+                },
+            };
+
+            this.plugin = new Y.eZ.Plugin.ContentType({host: this.service});
+        },
+
+        tearDown: function () {
+            this.service.destroy();
+            this.plugin.destroy();
+            delete this.service;
+            delete this.plugin;
+            delete this.capi;
+        },
+
+        _configureTypeService: function (groupsError) {
+            Mock.expect(this.typeService, {
+                method: 'loadContentTypeGroups',
+                args: [Mock.Value.Function],
+                run: Y.bind(function (callback) {
+                    callback(groupsError, !groupsError ? this.responseGroups : undefined);
+                }, this),
+            });
+        },
+
+        _getContentTypeGroupImpl: function (loadContentTypesError) {
+            return Y.Base.create('contentTypeGroupsModel', Y.Base, [], {
+                loadFromHash: function (hash) {
+                    this.hash = hash;
+                },
+
+                loadContentTypes: Y.bind(function (options, callback) {
+                    Assert.areSame(
+                        this.capi,
+                        options.api,
+                        "The CAPI should be passed"
+                    );
+                    callback(loadContentTypesError);
+                }, this),
+            });
+        },
+
+        "Should handle Content Type groups loading error": function () {
+            var groupsError = {},
+                callbackCalled = false;
+
+            this._configureTypeService(groupsError);
+            this.plugin.loadAllContentTypes(function (error) {
+                callbackCalled = true;
+                Assert.areSame(
+                    groupsError, error,
+                    "The groups loading error should be provided"
+                );
+            });
+            Assert.isTrue(
+                callbackCalled,
+                "The loadAllContentTypes should have been called"
+            );
+        },
+
+        "Should create each group and load the corresponding Content Types": function () {
+            var callbackCalled = false;
+
+            this._configureTypeService();
+            this.plugin._set('contentTypeGroupConstructor', this._getContentTypeGroupImpl(false));
+            this.plugin.loadAllContentTypes(Y.bind(function (error, groups) {
+                callbackCalled = true;
+
+                Assert.isFalse(error, "No error should be reported");
+                Assert.areEqual(
+                    this.responseGroups.document.ContentTypeGroupList.ContentTypeGroup.length,
+                    groups.length,
+                    "The groups list should have been loaded"
+                );
+                groups.forEach(function (group, i) {
+                    Assert.areEqual(
+                        this.responseGroups.document.ContentTypeGroupList.ContentTypeGroup[i]._href,
+                        group.get('id'),
+                        "The group should have receive the id from the group list response"
+                    );
+                    Assert.areSame(
+                        this.responseGroups.document.ContentTypeGroupList.ContentTypeGroup[i],
+                        group.hash,
+                        "The group should be the result of the hash parsing"
+                    );
+                }, this);
+            }, this));
+            Assert.isTrue(
+                callbackCalled,
+                "The loadAllContentTypes should have been called"
+            );
+        },
+
+        "Should handle Content Types loading error": function () {
+            var callbackCalled = false;
+
+            this._configureTypeService();
+            this.plugin._set('contentTypeGroupConstructor', this._getContentTypeGroupImpl(true));
+            this.plugin.loadAllContentTypes(Y.bind(function (error, groups) {
+                callbackCalled = true;
+
+                Assert.isTrue(error, "The Content Type loading error should be handled");
+            }, this));
+            Assert.isTrue(
+                callbackCalled,
+                "The loadAllContentTypes should have been called"
+            );
+        },
+    });
+
+    Y.Test.Runner.setName('eZ Content Create Plugin tests');
+    Y.Test.Runner.add(loadAllContentTypesTest);
+}, '', {
+    requires: ['test', 'base', 'ez-contenttypeplugin', 'ez-viewservice']
+});
