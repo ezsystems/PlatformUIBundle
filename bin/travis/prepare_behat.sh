2118e1587d5c04b7d64b057696c83c539deed1e7
André R <ar@ez.no>
2016-08-09 23:12:17 +0200

    [Travis] Run using Trusty images & docker images for Behat to get tests to pass
    As part of this:
    - use system composer *(it's uptodate now)*, and hence simplify some commands
    - use system nvm *(it's at v0.31.0)*
    - align with kernel on how behat tests are run using Docker images that are more optimized *(real setup using nginx instead of php's builtin server, more optimizede php setup, ..)*
    
    Also had to downgrade phantomjs to v1.9.8, as the 2.x versions does not seem to work with grover.

--- bin/travis/prepare_behat.sh
+++ bin/travis/prepare_behat.sh
@@ -1,35 +1,17 @@
 #!/bin/sh
 
-# File for setting up system for behat testing, just like done in DemoBundle's .travis.yml
+# File for setting up system for Behat testing
+
+set -e
 
 # Change local git repo to be a full one as we will reuse it for composer install below
-git fetch --unshallow && git checkout -b tmp_travis_branch
+git fetch --unshallow && git checkout -b tmp_ci_branch
+export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR TRAVIS_BUILD_DIR="$HOME/build/ezplatform"
 cd "$HOME/build"
 
 # Checkout meta repo, change the branch and/or remote to use a different ezpublish branch/distro
-git clone --depth 1 --single-branch --branch 1.4 https://github.com/ezsystems/ezplatform.git
+git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezplatform.git
 cd ezplatform
 
-./bin/.travis/disable_xdebug.sh
-./bin/.travis/configure_mysql.sh
-./bin/.travis/prepare_selenium2.sh
-
-echo "> Modify composer.json to point to local checkout"
-sed -i '$d' composer.json
-echo ',    "repositories": [{"type":"git","url":"'$TRAVIS_BUILD_DIR'"}]}' >> composer.json
-
-echo "> Updating packages (ezsystems/platform-ui-bundle:dev-tmp_travis_branch as 1.5)"
-composer require --no-update "ezsystems/platform-ui-bundle:dev-tmp_travis_branch as 1.5"
-
-./bin/.travis/prepare_ezpublish.sh
-
-echo "> Running the server"
-php app/console server:start --router=bin/.travis/router_behat.php --env=behat
-
-echo "> Warm up cache, using curl to make sure everything is warmed up, incl class, http & spi cache"
-sleep 1
-curl -sSLI "http://127.0.0.1:8000"
-
-echo "> Configuring behat"
-cp behat.yml.dist behat.yml
-sed -i "s@base_url: 'http://localhost'@base_url: 'http://127.0.0.1:8000'@g" behat.yml
+# Install everything needed for behat testing, using our local branch of this repo
+./bin/.travis/trusty/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/platform-ui-bundle:dev-tmp_ci_branch"
