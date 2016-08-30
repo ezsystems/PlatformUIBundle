0aab4856a924b02982a37f942a42546c59ee9ee0
Damien Pobel <dp@ez.no>
2016-08-30 11:30:04 +0200

    EZP-26179: Intercept 'error' notification to check the session

--- Resources/public/js/apps/plugins/ez-notificationhubplugin.js
+++ Resources/public/js/apps/plugins/ez-notificationhubplugin.js
@@ -29,9 +29,65 @@ YUI.add('ez-notificationhubplugin', function (Y) {
                 app.showSideView('notificationHub');
             });
 
-            app.on('*:notify', function (e) {
-                app.showSideView('notificationHub', {notification: e.notification});
+            app.on('*:notify', Y.bind(function (e) {
+                var notification = e.notification;
+
+                if ( notification.state === 'error' ) {
+                    this._checkIsLoggedIn(
+                        this._logOut,
+                        Y.bind(this._showNotification, this, e.notification)
+                    );
+                } else {
+                    this._showNotification(e.notification);
+                }
+            }, this));
+        },
+
+        /**
+         * Checks if the user is logged in and call the corresponding callback.
+         *
+         * @private
+         * @method _checkIsLoggedIn
+         * @param {Function} notLoggedInCallback
+         * @param {Function} loggedInCallback
+         */
+        _checkIsLoggedIn: function (notLoggedInCallback, loggedInCallback) {
+            var app = this.get('host');
+
+            app.set('loading', true);
+            app.isLoggedIn(Y.bind(function (notLoggedIn) {
+                if ( notLoggedIn ) {
+                    notLoggedInCallback.apply(this);
+                    return;
+                }
+                app.set('loading', false);
+                loggedInCallback.apply(this);
+            }, this));
+        },
+
+        /**
+         * Logs out the user and redirect to the login form.
+         *
+         * @method _logOut
+         * @private
+         */
+        _logOut: function () {
+            var app = this.get('host');
+
+            app.logOut(function () {
+                app.navigateTo('loginForm');
             });
+        },
+
+        /**
+         * Shows the notification in the notification hub.
+         *
+         * @method _showNotification
+         * @param {Object} notification
+         * @protected
+         */
+        _showNotification: function (notification) {
+            this.get('host').showSideView('notificationHub', {notification: notification});
         },
     }, {
         NS: 'notificationHub',
