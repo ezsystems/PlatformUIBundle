471b276e6ddb28b93b7671354bb95a64f1c64620
Bertrand Dunogier <bertrand.dunogier@ez.no>
2016-10-07 10:09:04 +0200

    fixup! Proof of concept of bundle generator

--- Generator/PlatformUIPluginGenerator.php
+++ Generator/PlatformUIPluginGenerator.php
@@ -22,7 +22,7 @@ class PlatformUIPluginGenerator extends Generator
 
     public function generate($namespace, $bundle, $dir, $format, $structure)
     {
-        $dir .= '/'.strtr($namespace, '\\', '/');
+        $dir .= '/' . strtr($namespace, '\\', '/');
         if (file_exists($dir)) {
             if (!is_dir($dir)) {
                 throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" exists but is a file.', realpath($dir)));
@@ -39,33 +39,33 @@ class PlatformUIPluginGenerator extends Generator
         $basename = substr($bundle, 0, -6);
         $parameters = array(
             'namespace' => $namespace,
-            'bundle'    => $bundle,
-            'format'    => $format,
+            'bundle' => $bundle,
+            'format' => $format,
             'bundle_basename' => $basename,
             'extension_alias' => Container::underscore($basename),
         );
 
         if ('xml' === $format || 'annotation' === $format) {
-            $this->renderFile('bundle/yui.xml.twig', $dir.'/Resources/config/yui.xml', $parameters);
-            $this->renderFile('bundle/css.xml.twig', $dir.'/Resources/config/css.xml', $parameters);
+            $this->renderFile('bundle/yui.xml.twig', $dir . '/Resources/config/yui.xml', $parameters);
+            $this->renderFile('bundle/css.xml.twig', $dir . '/Resources/config/css.xml', $parameters);
         } else {
-            $this->renderFile('bundle/yui.'.$format.'.twig', $dir.'/Resources/config/yui.'.$format, $parameters);
-            $this->renderFile('bundle/css.'.$format.'.twig', $dir.'/Resources/config/css.'.$format, $parameters);
+            $this->renderFile('bundle/yui.' . $format . '.twig', $dir . '/Resources/config/yui.' . $format, $parameters);
+            $this->renderFile('bundle/css.' . $format . '.twig', $dir . '/Resources/config/css.' . $format, $parameters);
         }
 
-        $this->renderFile('bundle/Bundle.php.twig', $dir.'/'.$bundle.'.php', $parameters);
-        $this->renderFile('bundle/PlatformUIPluginExtension.php.twig', $dir.'/DependencyInjection/'.$basename.'Extension.php', $parameters);
-        $this->renderFile('bundle/Configuration.php.twig', $dir.'/DependencyInjection/Configuration.php', $parameters);
+        $this->renderFile('bundle/Bundle.php.twig', $dir . '/' . $bundle . '.php', $parameters);
+        $this->renderFile('bundle/PlatformUIPluginExtension.php.twig', $dir . '/DependencyInjection/' . $basename . 'Extension.php', $parameters);
+        $this->renderFile('bundle/Configuration.php.twig', $dir . '/DependencyInjection/Configuration.php', $parameters);
 
         if ($structure) {
-            $this->renderFile('bundle/messages.fr.xlf', $dir.'/Resources/translations/messages.fr.xlf', $parameters);
+            $this->renderFile('bundle/messages.fr.xlf', $dir . '/Resources/translations/messages.fr.xlf', $parameters);
 
-            $this->filesystem->mkdir($dir.'/Resources/doc');
-            $this->filesystem->touch($dir.'/Resources/doc/index.rst');
-            $this->filesystem->mkdir($dir.'/Resources/translations');
-            $this->filesystem->mkdir($dir.'/Resources/public/css');
-            $this->filesystem->mkdir($dir.'/Resources/public/images');
-            $this->filesystem->mkdir($dir.'/Resources/public/js');
+            $this->filesystem->mkdir($dir . '/Resources/doc');
+            $this->filesystem->touch($dir . '/Resources/doc/index.rst');
+            $this->filesystem->mkdir($dir . '/Resources/translations');
+            $this->filesystem->mkdir($dir . '/Resources/public/css');
+            $this->filesystem->mkdir($dir . '/Resources/public/images');
+            $this->filesystem->mkdir($dir . '/Resources/public/js');
         }
     }
 }
