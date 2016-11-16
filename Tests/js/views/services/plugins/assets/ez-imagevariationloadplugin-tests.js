4b5742b96208584ad672913c42d4c7a39ca4eea9
Damien Pobel <dp@ez.no>
2016-11-16 15:33:54 +0100

    EZP-26322: Make sure image fields are correctly rendered in the content peek view

--- Tests/js/views/services/plugins/assets/ez-imagevariationloadplugin-tests.js
+++ Tests/js/views/services/plugins/assets/ez-imagevariationloadplugin-tests.js
@@ -212,7 +212,10 @@ YUI.add('ez-imagevariationloadplugin-tests', function (Y) {
 
     registerTest = new Y.Test.Case(Y.eZ.Test.PluginRegisterTest);
     registerTest.Plugin = Y.eZ.Plugin.ImageVariationLoad;
-    registerTest.components = ['locationViewViewService', 'universalDiscoveryViewService', 'contentEditViewService', 'contentCreateViewService'];
+    registerTest.components = [
+        'locationViewViewService', 'universalDiscoveryViewService', 'contentEditViewService',
+        'contentCreateViewService', 'contentPeekViewService'
+    ];
 
     Y.Test.Runner.setName("eZ Image Variation Load Plugin tests");
     Y.Test.Runner.add(defaultCallbackTest);
