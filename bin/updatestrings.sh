5298e0c74fadf429ddc58ac8445819ec4f02cbc2
Nicolas Bastien <nicolas.bastien@ez.no>
2017-01-13 16:46:38 +0100

    EZP-26835 : Remove translation string extraction as it will be handle in ezplatform

--- bin/updatestrings.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-../../../app/console translation:extract en -v \
-  --dir=. \
-  --exclude-dir=vendor --exclude-dir=Tests --exclude-dir=Features --exclude-dir=node_modules --exclude-dir=Resources/public/vendors \
-  --output-dir=./Resources/translations \
-  --keep
-  "$@"
