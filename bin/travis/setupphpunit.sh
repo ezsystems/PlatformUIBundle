66a34789dbc7f50a572b7f474010c7dde9db5e7b
Andrew Longosz <andrzej.longosz@ez.no>
2017-01-23 14:13:24 +0100

    EZP-26881: Allowed running PHPUnit tests with different versions of Sf

--- bin/travis/setupphpunit.sh
+++ bin/travis/setupphpunit.sh
@@ -10,5 +10,11 @@ nvm use
 echo "> installing node packages"
 npm install
 
+# Switch to another Symfony version if asked for
+if [ "$SYMFONY_VERSION" != "" ] ; then
+    echo "> Update symfony/symfony requirement to ${SYMFONY_VERSION}"
+    composer require --no-update symfony/symfony="${SYMFONY_VERSION}"
+fi
+
 echo "> Running composer install"
 composer install --prefer-dist
