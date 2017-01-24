47c0cc3bdb8a7ab97273548277ea8cc6156dcab7
Damien Pobel <dp@ez.no>
2017-01-24 14:56:26 +0100

    EZP-26517: Make sure the linkEdit button is correctly positioned after using the UDW

--- Resources/public/js/alloyeditor/toolbars/config/link.js
+++ Resources/public/js/alloyeditor/toolbars/config/link.js
@@ -11,7 +11,9 @@ YUI.add('ez-alloyeditor-toolbar-config-link', function (Y) {
      */
     Y.namespace('eZ.AlloyEditorToolbarConfig');
 
-    var AlloyEditor = Y.eZ.AlloyEditor;
+    /* global CKEDITOR */
+    var AlloyEditor = Y.eZ.AlloyEditor,
+        ReactDOM = Y.eZ.ReactDOM;
 
     /**
      * `styles` toolbar configuration for links
@@ -22,7 +24,23 @@ YUI.add('ez-alloyeditor-toolbar-config-link', function (Y) {
     Y.eZ.AlloyEditorToolbarConfig.Link = {
         name: 'link',
         buttons: ['linkEdit'],
-        test: AlloyEditor.SelectionTest.link
+        test: AlloyEditor.SelectionTest.link,
+        getArrowBoxClasses: function () {
+            return 'ae-arrow-box ae-arrow-box-bottom';
+        },
+        setPosition: function (payload) {
+            var domElement = new CKEDITOR.dom.element(ReactDOM.findDOMNode(this)),
+                region = payload.selectionData.region,
+                xy = this.getWidgetXYPoint(
+                    region.left, region.top,
+                    CKEDITOR.SELECTION_BOTTOM_TO_TOP
+                );
+
+            domElement.addClass('ae-toolbar-transition');
+            domElement.setStyles({left: xy[0] + 'px', top: xy[1] + 'px'});
+
+            return true;
+        },
     };
 });
 
--- Tests/js/alloyeditor/toolbars/config/assets/ez-alloyeditor-toolbar-config-link-tests.js
+++ Tests/js/alloyeditor/toolbars/config/assets/ez-alloyeditor-toolbar-config-link-tests.js
@@ -3,8 +3,12 @@
  * For full copyright and license information view LICENSE file distributed with this source code.
  */
 YUI.add('ez-alloyeditor-toolbar-config-link-tests', function (Y) {
+    /* global CKEDITOR */
     var defineTest,
-        Link = Y.eZ.AlloyEditorToolbarConfig.Link;
+        getArrowBoxClassesTest, setPositionTest,
+        Link = Y.eZ.AlloyEditorToolbarConfig.Link,
+        ReactDOM = Y.eZ.ReactDOM,
+        Assert = Y.Assert, Mock = Y.Mock;
 
     defineTest = new Y.Test.Case(Y.merge(Y.eZ.Test.ToolbarConfigDefineTest, {
         name: 'eZ AlloyEditor link config toolbar define test',
@@ -13,8 +17,80 @@ YUI.add('ez-alloyeditor-toolbar-config-link-tests', function (Y) {
         methods: {
             test: Y.eZ.AlloyEditor.SelectionTest.link
         },
+
+        _should: {
+            ignore: {
+                "Should have the correct `setPosition` method": true,
+                "Should have the correct `getArrowBoxClasses` method": true,
+            },
+        },
     }));
+
+    getArrowBoxClassesTest = new Y.Test.Case({
+        name: 'eZ AlloyEditor link config toolbar getArrowBoxClasses test',
+
+        "Should return the arrow classes": function () {
+            Assert.areEqual(
+                'ae-arrow-box ae-arrow-box-bottom',
+                Link.getArrowBoxClasses(),
+                "The getArrowBoxClasses should return the arrow classes"
+            );
+        },
+    });
+
+    setPositionTest = new Y.Test.Case({
+        name: 'eZ AlloyEditor link config toolbar setPosition test',
+
+        setUp: function () {
+            this.toolbar = new Mock();
+            this.toolbarNode = document.querySelector('.toolbar');
+            this.origFindDOMNode = ReactDOM.findDOMNode;
+            ReactDOM.findDOMNode = Y.bind(function (toolbar) {
+                Assert.areSame(
+                    this.toolbar,
+                    toolbar
+                );
+                return this.toolbarNode;
+            }, this);
+            this.regionLeft = 22;
+            this.regionTop = 23;
+            this.xy = [42, 43];
+            Mock.expect(this.toolbar, {
+                method: 'getWidgetXYPoint',
+                args: [this.regionLeft, this.regionTop, CKEDITOR.SELECTION_BOTTOM_TO_TOP],
+                returns: this.xy,
+            });
+        },
+
+        "Should set the position of the toolbar": function () {
+            Link.setPosition.call(this.toolbar, {
+                selectionData: {
+                    region: {
+                        left: this.regionLeft,
+                        top: this.regionTop,
+                    }
+                }
+            });
+            Assert.areEqual(
+                this.xy[0] + 'px',
+                this.toolbarNode.style.left,
+                "The toolbar should be position horizontally"
+            );
+            Assert.areEqual(
+                this.xy[1] + 'px',
+                this.toolbarNode.style.top,
+                "The toolbar should be position vertically"
+            );
+            Assert.isTrue(
+                this.toolbarNode.classList.contains('ae-toolbar-transition'),
+                "The ae toolbar transition class should have been added"
+            );
+            Mock.verify(this.toolbar);
+        },
+    });
 
     Y.Test.Runner.setName("eZ AlloyEditor link config toolbar tests");
     Y.Test.Runner.add(defineTest);
+    Y.Test.Runner.add(getArrowBoxClassesTest);
+    Y.Test.Runner.add(setPositionTest);
 }, '', {requires: ['test', 'toolbar-config-define-tests', 'node', 'ez-alloyeditor-toolbar-config-link']});
