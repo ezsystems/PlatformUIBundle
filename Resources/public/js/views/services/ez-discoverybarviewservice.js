d58f329ae79ed5ddebe643479a22b74f5eef03df
stephaneDiot <stephane.diot@ez.no>
2017-02-21 09:47:19 +0100

    EZP-27008: Use UDW to browse content

--- Resources/public/js/views/services/ez-discoverybarviewservice.js
+++ Resources/public/js/views/services/ez-discoverybarviewservice.js
@@ -20,10 +20,58 @@ YUI.add('ez-discoverybarviewservice', function (Y) {
      * @extends eZ.ViewService
      */
     Y.eZ.DiscoveryBarViewService = Y.Base.create('discoveryBarViewService', Y.eZ.ViewService, [Y.eZ.SideViewService], {
-
         initializer: function () {
             this.on('*:viewTrashAction', this._redirectToTrashView);
             this.on('*:viewSearchAction', this._redirectToSearchView);
+            this.on('*:browseAction', this._fireContentDiscover);
+        },
+
+        /**
+         * Checks whether the currently active view is a LocationViewView.
+         *
+         * @method _isLocationViewDisplayed
+         * @protected
+         * @return {Boolean}
+         */
+        _isLocationViewDisplayed: function () {
+            return this.get('app').get('activeView') instanceof Y.eZ.LocationViewView;
+        },
+
+        /**
+         * Launch UDW to the current location view if there is one.
+         *
+         * @method _fireContentDiscover
+         * @protected
+         */
+        _fireContentDiscover: function () {
+            var startingLocationId;
+
+            if (this._isLocationViewDisplayed()) {
+               startingLocationId = this.get('app').get('activeView').get('location').get('id');
+            }
+
+            this.fire('contentDiscover', {
+                config: {
+                    title: 'Find location', //provisional
+                    multiple: false,
+                    contentDiscoveredHandler: Y.bind(this._navigateToLocation, this),
+                    startingLocationId: startingLocationId,
+                },
+            });
+        },
+
+        /**
+         * Navigate to the selected location.
+         *
+         * @method _navigateToLocation
+         * @param {eventFacade} e
+         * @protected
+         */
+        _navigateToLocation: function (e) {
+            this.get('app').navigateTo('viewLocation', {
+                    id: e.selection.location.get('id'),
+                    languageCode: e.selection.content.get('mainLanguageCode')
+            });
         },
 
         /**
