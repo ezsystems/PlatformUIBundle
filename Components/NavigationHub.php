9d5b741697317829a2614a80fce8454dda046995
Damien Pobel <dp@ez.no>
2017-03-03 16:35:01 +0100

    Implemented a working (in terms of navigation) Navigation Hub

--- Components/NavigationHub.php
+++ Components/NavigationHub.php
@@ -2,33 +2,93 @@
 
 namespace EzSystems\PlatformUIBundle\Components;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class NavigationHub extends Component
 {
     const TAG_NAME = 'ez-navigation-hub';
 
+    const ACTIVE_ZONE_CLASS = 'ez-active-zone';
+
+    const MATCHED_LINK_CLASS = 'ez-matched-link';
+
     protected $templating;
 
-    public function __construct($templating)
+    protected $zones;
+
+    protected $links;
+
+    public function __construct($templating, RequestStack $stack, array $zones, array $links)
     {
+        $this->request = $stack->getMasterRequest();
         $this->templating = $templating;
+        $this->zones = $zones;
+        $this->links = $links;
     }
 
     public function __toString()
     {
-        $str = '<' . self::TAG_NAME . '>';
-        $str .= $this->templating->render(
-            'eZPlatformUIBundle:Components:navigationhub.html.twig'
+        return $this->templating->render(
+            'eZPlatformUIBundle:Components:navigationhub.html.twig',
+            [
+                'tag' => self::TAG_NAME,
+                'attributes' => $this->getAttributes(),
+                'zones' => $this->zones,
+                'links' => $this->links,
+                'activeZone' => $this->getActiveZoneIdentifier(),
+                'activeZoneClass' => self::ACTIVE_ZONE_CLASS,
+                'matchedLinkClass' => self::MATCHED_LINK_CLASS,
+            ]
         );
-        $str .= '</' . self::TAG_NAME . '>';
+    }
 
-        return $str;
+    protected function getActiveLink()
+    {
+        foreach ($this->links as $link) {
+            if ($link->match($this->request)) {
+                return $link;
+            }
+        }
+
+        return null;
+    }
+
+    protected function getActiveZoneIdentifier()
+    {
+        $link = $this->getActiveLink();
+        if ($link) {
+            return $link->zone;
+        }
+        return '';
+    }
+
+    protected function getActiveLinkUrl()
+    {
+        $link = $this->getActiveLink();
+
+        if ($link) {
+            return $link->getUrl();
+        }
+        return '';
+    }
+
+    protected function getAttributes()
+    {
+        return [
+            'active-zone-class' => self::ACTIVE_ZONE_CLASS,
+            'matched-link-class' => self::MATCHED_LINK_CLASS,
+            'active-zone' => $this->getActiveZoneIdentifier(),
+            'matched-link-url' => $this->getActiveLinkUrl(),
+        ];
     }
 
     public function jsonSerialize()
     {
         return [
             'selector' => self::TAG_NAME,
-            'update' => (string)$this,
+            'update' => [
+                'attributes' => $this->getAttributes(),
+            ]
         ];
     }
 }
---
+++ NavigationHub/Link/Route.php
@@ -0,0 +1,57 @@
+<?php
+namespace EzSystems\PlatformUIBundle\NavigationHub\Link;
+
+use EzSystems\PlatformUIBundle\NavigationHub\Link;
+use Symfony\Component\HttpFoundation\Request;
+
+class Route extends Link
+{
+    protected $router;
+
+    protected $routeName;
+
+    protected $routeParams;
+
+    public function __construct($router, $routeName, $routeParams, $name, $zoneIdentifier)
+    {
+        $this->router = $router;
+        $this->zone = $zoneIdentifier;
+        $this->name = $name;
+        $this->routeName = $routeName;
+        $this->routeParams = $routeParams;
+    }
+
+    public function getUrl()
+    {
+        return $this->router->generate($this->routeName, $this->routeParams);
+    }
+
+    public function match(Request $request)
+    {
+        $routeName = $request->attributes->get('_route');
+        $routeParams = $request->attributes->get('_routeParams');
+
+        return (
+            $this->matchRoute($routeName)
+            && $this->matchRouteParams($routeParams)
+        );
+    }
+
+    protected function matchRoute($routeName)
+    {
+        return $routeName === $this->routeName;
+    }
+
+    protected function matchRouteParams($params)
+    {
+        if (count($params) !== count($this->routeParams)) {
+            return false;
+        }
+        foreach ($params as $name => $value) {
+            if (!isset($this->routeParams[$name]) || $this->routeParams[$name] !== $value) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
