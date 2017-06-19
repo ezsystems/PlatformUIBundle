2bd820b0d45b2ce7ca393771b2478b8a2f28f551
stephaneDiot <stephane.diot@ez.no>
2017-06-19 10:16:02 +0200

    EZP-27352: implement <ez-universal-discovery>

---
+++ Resources/public/webcomponents/js/ez-universal-discovery.js
@@ -0,0 +1,142 @@
+(function () {
+
+    /**
+     * `<ez-universal-discovery>` generates the universal discovery widget.
+     *
+     * @demo demo/ez-universal-discovery.html
+     * @polymerElement
+     */
+    class UniversalDiscoveryWidget extends eZ.SandboxYUIComponentMixin(Polymer.Element) {
+        static get is() {
+            return 'ez-universal-discovery';
+        }
+        static get properties() {
+            return {
+                /**
+                 * Title of the universal discovery widget.
+                 */
+                title: {
+                    type: String,
+                },
+
+                /**
+                 * Flag indicating whether the user is able to select several
+                 * Content items.
+                 */
+                multiple: {
+                    type: Boolean,
+                    value: false,
+                },
+
+                /**
+                 * The Location that should be considered as the starting point when
+                 * discovering Content.
+                 */
+                startingLocationId: {
+                    type: String,
+                },
+
+                /**
+                 * Defines the label of the confirm button.
+                 */
+                confirmLabel: {
+                    type: String,
+                },
+
+                /**
+                * The depth of the root where we start discovering content.
+                * The UDW needs a starting location having a greater depth than the min discover depth to work.
+                 */
+                minDiscoverDepth: {
+                    type: Number,
+                },
+
+                /**
+                 * The identifier of the visible method by default.
+                 */
+                visibleMethod: {
+                    type: String,
+                },
+            };
+        }
+
+        connectedCallback() {
+            super.connectedCallback();
+            this._trackYUIAppReady(this._createUDWComponent.bind(this));
+        }
+
+        /**
+         * Converts a selection items to a YUI free representation by calling toObject on each model object.
+         *
+         * @param {Object} item
+         */
+        _convertItem(item) {
+            return {
+                content: item.content.toObject(),
+                location: item.location.toObject(),
+                contentType: item.contentType.toObject(),
+            };
+        }
+
+        /**
+         * Converts the selection to a YUI free representation.
+         *
+         * @param {Array|Object} selection
+         */
+        _convertSelection(selection) {
+            if ( this.multiple ) {
+                return selection.map(this._convertItem);
+            }
+            return this._convertItem(selection);
+        }
+
+        /**
+         * Creates the universal discovery widget component and renders the UD side view using the YUI bridge app.
+         */
+        _createUDWComponent() {
+            const {app, Y} = eZ.YUI;
+            const params = {
+                title: this.title,
+                multiple: this.multiple,
+                startingLocationId: this.startingLocationId,
+                minDiscoverDepth: this.minDiscoverDepth,
+                visibleMethod: this.visibleMethod,
+                contentDiscoveredHandler: (e) => {
+                    this.dispatchEvent(new CustomEvent('ez:confirm', {
+                        detail: {
+                            selection: this._convertSelection(e.selection),
+                        },
+                        bubbles: false,
+                    }));
+                },
+                cancelDiscoverHandler: (e) => {
+                    this.dispatchEvent(new CustomEvent('ez:cancel', {
+                        bubbles: false,
+                    }));
+                },
+                isSelectable: (selection) => {
+                    return this.dispatchEvent(new CustomEvent('ez:select', {
+                        detail: {
+                            selection: this._convertItem(selection),
+                        },
+                        bubbles: false,
+                        cancelable: true,
+                    }));
+                },
+            };
+
+            if ( this.confirmLabel ) {
+                params.confirmLabel = this.confirmLabel;
+            }
+
+            app.renderSideView(
+                Y.eZ.UniversalDiscoveryView,
+                Y.eZ.UniversalDiscoveryViewService,
+                params,
+                this._attachView.bind(this)
+            );
+        }
+    }
+
+    customElements.define(UniversalDiscoveryWidget.is, UniversalDiscoveryWidget);
+})();
---
+++ Tests/webcomponents/js/ez-universal-discovery.js
@@ -0,0 +1,409 @@
+/* global describe, it, beforeEach, fixture, assert, afterEach, sinon */
+describe('ez-universal-discovery', function () {
+    let universalDiscovery;
+    let universalDiscoveryMultiple;
+    const modelObject = 'hasBeenTransformedToModelObject';
+
+    function getAppMock() {
+        const App = function () {
+            this.renderSideView = function () {};
+        };
+
+        return new App();
+    }
+
+    function getViewMock(domNode) {
+        const View = function () {
+            this.set = function () {};
+            this.get = function () {
+                return {
+                    getDOMNode: function () {
+                        return domNode;
+                    },
+                };
+            };
+            this.destroy = function () {};
+        };
+
+        return new View();
+    }
+
+    function defineGlobals() {
+        window.eZ = {
+            YUI: {
+                app: getAppMock(),
+                Y: {
+                    eZ: {
+                        UniversalDiscoveryView: function () {},
+                        UniversalDiscoveryViewService: function () {},
+                    }
+                }
+            }
+        };
+    }
+
+    function getModelMock() {
+        const Model = function () {
+            this.toObject = function () {
+                return modelObject;
+            };
+        };
+
+        return new Model();
+    }
+
+    function dispatchEvent(handlerName, selection) {
+        const container = document.createElement('div');
+        const view = getViewMock(container);
+
+        renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+            params[handlerName](selection);
+            done(false, 'whatever', view);
+        });
+        document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+    }
+
+    it('should be defined', function () {
+        universalDiscovery = fixture('BasicTestFixture');
+        assert.equal(
+            window.customElements.get('ez-universal-discovery'),
+            universalDiscovery.constructor
+        );
+    });
+
+    describe('properties', function () {
+        beforeEach(function () {
+            universalDiscovery = fixture('BasicTestFixture');
+            universalDiscoveryMultiple = fixture('MultipleTestFixture');
+        });
+
+        it('should have a `title` property', function () {
+            assert.equal(
+                universalDiscovery.getAttribute('title'),
+                universalDiscovery.title
+            );
+        });
+
+        it('should have a `startingLocationId` property', function () {
+            assert.equal(
+                universalDiscovery.getAttribute('starting-location-id'),
+                universalDiscovery.startingLocationId
+            );
+        });
+
+        it('should have a `confirmLabel` property', function () {
+            assert.equal(
+                universalDiscovery.getAttribute('confirm-label'),
+                universalDiscovery.confirmLabel
+            );
+        });
+
+        it('should have a `minDiscoverDepth` property', function () {
+            assert.equal(
+                universalDiscovery.getAttribute('min-discover-depth'),
+                universalDiscovery.minDiscoverDepth
+            );
+        });
+
+        it('should have a `visibleMethod` property', function () {
+            assert.equal(
+                universalDiscovery.getAttribute('visible-method'),
+                universalDiscovery.visibleMethod
+            );
+        });
+        describe('`multiple` property', function () {
+            it('should have a `multiple` property', function () {
+                assert.equal(
+                    universalDiscoveryMultiple.hasAttribute('multiple'),
+                    universalDiscoveryMultiple.multiple
+                );
+            });
+
+            it('`multiple` property should be false by default', function () {
+                assert.isFalse(
+                    universalDiscovery.multiple
+                );
+            });
+        });
+    });
+
+    describe('YUI stack loading', function () {
+        let renderViewSpy;
+
+        function assertViewRendered(spy, Y) {
+            assert.isOk(
+                spy.callCount
+            );
+            assert.equal(
+                spy.firstCall.args[0],
+                Y.eZ.UniversalDiscoveryView,
+                'The UniversalDiscoveryView should have been rendered'
+            );
+        }
+
+        afterEach(function () {
+            delete window.eZ;
+            renderViewSpy.restore();
+        });
+
+        describe('`ez-yui-app:ready` event', function () {
+            beforeEach(function () {
+                universalDiscovery = fixture('BasicTestFixture');
+                defineGlobals();
+                renderViewSpy = sinon.spy(window.eZ.YUI.app, 'renderSideView');
+            });
+
+            it('should render the universal discovery', function () {
+                document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+
+                assertViewRendered(renderViewSpy, window.eZ.YUI.Y);
+            });
+        });
+
+        describe('already loaded', function () {
+            beforeEach(function () {
+                defineGlobals();
+                renderViewSpy = sinon.spy(window.eZ.YUI.app, 'renderSideView');
+                universalDiscovery = fixture('BasicTestFixture');
+            });
+
+            it('should render the universal discovery right away', function () {
+                assertViewRendered(renderViewSpy, window.eZ.YUI.Y);
+            });
+        });
+    });
+
+    describe('YUI Universal Discovery Widget', function () {
+        let renderViewStub;
+        const container = document.createElement('div');
+        const location = getModelMock();
+        const content = getModelMock();
+        const contentType = getModelMock();
+
+        function assertEventParam(e) {
+            assert.equal(e.detail.selection.content, modelObject);
+            assert.equal(e.detail.selection.location, modelObject);
+            assert.equal(e.detail.selection.contentType, modelObject);
+            assert.isFalse(e.bubbles);
+        }
+
+        beforeEach(function () {
+            universalDiscovery = fixture('BasicTestFixture');
+            defineGlobals();
+        });
+
+        afterEach(function () {
+            delete window.eZ;
+            renderViewStub.restore();
+        });
+
+        it('should append the YUI view container', function () {
+            const view = getViewMock(container);
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                done(false, 'whatever', view);
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+            assert.equal(
+                container.parentNode,
+                universalDiscovery
+            );
+        });
+
+        it('should have received parameters from the properties', function () {
+            const view = getViewMock(container);
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                assert.equal(
+                    universalDiscovery.title,
+                    params.title,
+                    "The title should be passed in the parameters"
+                );
+
+                assert.equal(
+                    universalDiscovery.multiple,
+                    params.multiple,
+                    "The multiple should be passed in the parameters"
+                );
+
+                assert.equal(
+                    universalDiscovery.visibleMethod,
+                    params.visibleMethod,
+                    "The visibleMethod should be passed in the parameters"
+                );
+
+                assert.equal(
+                    universalDiscovery.confirmLabel,
+                    params.confirmLabel,
+                    "The confirmLabel should be passed in the parameters"
+                );
+
+                assert.equal(
+                    universalDiscovery.startingLocationId,
+                    params.startingLocationId,
+                    "The startingLocationId should be passed in the parameters"
+                );
+
+                assert.equal(
+                    universalDiscovery.minDiscoverDepth,
+                    params.minDiscoverDepth,
+                    "The minDiscoverDepth should be passed in the parameters"
+                );
+                done(false, 'whatever', view);
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+        });
+
+        it('should have received a cancelDiscoverHandler function', function () {
+            const view = getViewMock(container);
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                assert.isFunction(params.cancelDiscoverHandler);
+                done(false, 'whatever', view);
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+        });
+
+        it('should have received a contentDiscoveredHandler function', function () {
+            const view = getViewMock(container);
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                assert.isFunction(params.contentDiscoveredHandler);
+                done(false, 'whatever', view);
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+        });
+
+        it('should have received a isSelectable function', function () {
+            const view = getViewMock(container);
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                assert.isFunction(params.isSelectable);
+                done(false, 'whatever', view);
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+        });
+
+        it('should handle loading error', function () {
+            const errorMsg = 'error message';
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                done(new Error(errorMsg));
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+
+            assert.equal(
+                universalDiscovery.innerHTML,
+                errorMsg,
+                '<ez-universal-discovery> should contain the error message'
+            );
+        });
+
+        it('should add the ez-js-standard-form class', function () {
+            const view = getViewMock(container);
+
+            renderViewStub = sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                done(false, 'whatever', view);
+            });
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+            assert.isTrue(universalDiscovery.classList.contains('ez-js-standard-form'));
+        });
+
+        describe('events', function () {
+
+            describe('`ez:cancel`', function () {
+                it('should be dispatched', function() {
+                    let isDispatched = false;
+
+                    universalDiscovery.addEventListener('ez:cancel', function (e) {
+                        assert.isFalse(e.bubbles);
+                        isDispatched = true;
+                    });
+                    dispatchEvent('cancelDiscoverHandler', {});
+                    assert.isTrue(isDispatched);
+                });
+            });
+
+            describe('`ez:select`', function () {
+                it('should be dispatched when selecting an item', function() {
+                    const selection = {location: location, content: content, contentType: contentType};
+                    let isDispatched = false;
+
+                    universalDiscovery.addEventListener('ez:select', function (e) {
+                        assertEventParam(e);
+                        isDispatched = true;
+                    });
+                    dispatchEvent('isSelectable', selection);
+                    assert.isTrue(isDispatched);
+                });
+
+                it('should allow to prevent the selection', function() {
+                    const selection = {location: location, content: content, contentType: contentType};
+
+                    universalDiscovery.addEventListener('ez:select', function (e) {
+                        assert.isTrue(e.cancelable);
+                    });
+                    dispatchEvent('isSelectable', selection);
+                });
+            });
+
+            describe('`ez:contentDiscovered`', function () {
+                it('should be dispatched with a simple selection', function() {
+                    const fakeEventFacade = {selection : {location: location, content: content, contentType: contentType}};
+                    let isDispatched = false;
+
+                    universalDiscovery.addEventListener('ez:confirm', function (e) {
+                        assertEventParam(e);
+                        isDispatched = true;
+                    });
+                    dispatchEvent('contentDiscoveredHandler', fakeEventFacade);
+                    assert.isTrue(isDispatched);
+                });
+
+                it('should be dispatched with a multiple selection', function() {
+                    const fakeEventFacade = {selection : [{location: location, content: content, contentType: contentType}]};
+                    let isDispatched = false;
+
+                    universalDiscovery.multiple = true;
+                    universalDiscovery.addEventListener('ez:confirm', function (e) {
+                        assert.equal(e.detail.selection[0].content, modelObject);
+                        assert.equal(e.detail.selection[0].location, modelObject);
+                        assert.equal(e.detail.selection[0].contentType, modelObject);
+                        isDispatched = true;
+                    });
+                    dispatchEvent('contentDiscoveredHandler', fakeEventFacade);
+                    assert.isTrue(isDispatched);
+                });
+            });
+        });
+    });
+
+    describe('disconnect', function () {
+        const container = document.createElement('div');
+        const view = getViewMock(container);
+
+        beforeEach(function () {
+            universalDiscovery = fixture('BasicTestFixture');
+            defineGlobals();
+            sinon.stub(window.eZ.YUI.app, 'renderSideView', function (View, Service, params, done) {
+                done(false, 'whatever', view);
+            });
+
+            document.dispatchEvent(new CustomEvent('ez:yui-app:ready'));
+        });
+
+        it('should set the YUI view inactive', function () {
+            const setSpy = sinon.spy(view, 'set').withArgs('active', false);
+
+            universalDiscovery.parentNode.removeChild(universalDiscovery);
+            assert.ok(setSpy.calledOnce);
+        });
+
+        it('should destroy the YUI view', function () {
+            const destroySpy = sinon.spy(view, 'destroy').withArgs({remove: true});
+
+            universalDiscovery.parentNode.removeChild(universalDiscovery);
+            assert.ok(destroySpy.calledOnce);
+        });
+    });
+});
